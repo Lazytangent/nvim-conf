03eac555581922271f4137b222be3803740098b7
Lazytangent <pm123633@gmail.com>
2021-05-15 17:05:44 -0500

    Setting values to variables for ease of access

--- lua/basic/autocmds.lua
+++ lua/basic/autocmds.lua
@@ -3,18 +3,23 @@ local u = require("utils.core")
 local autocmds = {
   filetypes = {
     {"FileType", "gitcommit", "setlocal textwidth=72"},
-    {"FileType", "python", "syntax keyword pythonDecorator True None False self"},
-    {"BufNewFile,BufRead", "*.py", "setlocal softtabstop=4 shiftwidth=4 expandtab textwidth=79"},
-    {"FileType", "python", [[let b:AutoPairs = AutoPairsDefine({"f'": "'", "r'": "'", "b'": "'"})]]},
     {"BufNewFile,BufRead", "*.jinja", "set syntax=htmljinja"},
     {"BufNewFile,BufRead", "*.mako", "set filetype=mako"},
+    {"FileType", "python", "syntax keyword pythonDecorator True None False self"},
+    {"FileType", "python", [[let b:AutoPairs = AutoPairsDefine({"f'": "'", "r'": "'", "b'": "'"})]]},
+    {"BufNewFile,BufRead", "*.py", "setlocal softtabstop=4 shiftwidth=4 expandtab textwidth=79"},
+
     {"FileType", "javascript", "setlocal fen"},
     {"FileType", "javascript", "setlocal nocindent"},
+
     {"BufRead", "*.twig", "set syntax=html filetype=html"},
+
     {"FileType", "markdown", "setlocal shiftwidth=4 softtabstop=4 expandtab"},
     {"BufNewFile,BufRead", "*.md", "setlocal textwidth=80"},
     {"BufNewFile,BufReadPost", "*.md", "set filetype=markdown"},
+
     {"BufNewFile,BufRead", "*.rasi", "set filetype=css"},
+
     {"FileType", "go", "setlocal shiftwidth=4 softtabstop=4"},
   },
   hl_yank = {
