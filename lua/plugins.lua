624b970814d3e6d3cc676ad6070db8dd7ec0719a
Lazytangent <pm123633@gmail.com>
2021-05-17 10:01:13 -0500

    More plugin config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,7 +22,7 @@ return require("packer").startup(
 
     -- Specific Language Plugins
     use {
-      { 'heavenshell/vim-jsdoc', run = 'make install', ft = {'js', 'jsx', 'ts', 'tsx', 'javascript', 'typescript'} },
+      { 'heavenshell/vim-jsdoc', run = 'make install', },
       'pantharshit00/vim-prisma',
       'leafgarland/typescript-vim',
       'peitalin/vim-jsx-typescript',
