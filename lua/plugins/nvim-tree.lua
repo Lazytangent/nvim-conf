0c053ca4380c93b757e6cb884a82b45d6bfdc785
Lazytangent <pm123633@gmail.com>
2021-07-08 09:41:34 -0500

    Don't have nvim-tree open automatically

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,5 +2,5 @@
 vim.g.nvim_tree_side = 'right'
 vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', '*.pyc'}
 vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_auto_open = 1
+vim.g.nvim_tree_auto_open = 0
 vim.g.nvim_tree_auto_close = 1
