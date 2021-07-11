66b12981f6641fe128e11db8fa0738bda9c5c480
Lazytangent <pm123633@gmail.com>
2021-07-11 11:08:42 -0500

    Migrating back to NERDTree

--- lua/basic/autocmds.lua
+++ lua/basic/autocmds.lua
@@ -41,6 +41,16 @@ u.define_augroups(autocmds)
 
 vim.cmd("autocmd FocusGained,BufEnter * checktime")
 vim.cmd("autocmd BufWritePost plugins.lua PackerCompile")
+vim.cmd([[
+  autocmd StdinReadPre * let s:std_in=1
+  autocmd VimEnter * if argc() == 0 && !exists('s:std_in') | NERDTree | endif
+]])
+vim.cmd([[
+  autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
+]])
+vim.cmd([[
+  autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 | let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
+]])
 -- vim.cmd("highlight ColorColumn ctermbg=magenta")
 -- vim.cmd("call matchadd('ColorColumn', '%81v', 100)")
 -- vim.fn.matchadd('ColorColumn', '%81v', 100)
