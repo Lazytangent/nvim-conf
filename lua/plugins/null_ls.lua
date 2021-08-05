7d19c856048d26ca99e2c7a26d4c2ae6055a1d87
Lazytangent <pm123633@gmail.com>
2021-08-05 15:28:35 -0500

    Update null_ls options

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -3,9 +3,13 @@ local null_ls = require('null-ls')
 local sources = {
   null_ls.builtins.formatting.eslint_d,
   null_ls.builtins.formatting.json_tool,
+  null_ls.builtins.formatting.stylua,
   null_ls.builtins.formatting.rustfmt,
   null_ls.builtins.formatting.sqlformat,
   null_ls.builtins.formatting.yapf,
+  null_ls.builtins.formatting.trim_whitespace.with({
+    filetypes = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
+  }),
 
   null_ls.builtins.diagnostics.eslint.with({
     command = "eslint_d"
