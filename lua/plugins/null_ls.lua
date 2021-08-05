4a78ccf044c1239046e067125d0f9c248ba4d9fe
Lazytangent <pm123633@gmail.com>
2021-08-05 14:35:59 -0500

    Update null_ls settings

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,16 +1,15 @@
 local null_ls = require('null-ls')
 
 local sources = {
-  null_ls.builtins.formatting.black,
   null_ls.builtins.formatting.eslint_d,
-  null_ls.builtins.formatting.isort,
   null_ls.builtins.formatting.json_tool,
-  null_ls.builtins.formatting.prettierd.with({
-    args = { "$FILENAME", "--single-quote", "--trailing-comma all" }
-  }),
   null_ls.builtins.formatting.rustfmt,
   null_ls.builtins.formatting.sqlformat,
   null_ls.builtins.formatting.yapf,
+
+  null_ls.builtins.diagnostics.eslint.with({
+    command = "eslint_d"
+  }),
 }
 
 null_ls.config({ sources = sources })
