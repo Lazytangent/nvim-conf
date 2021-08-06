7c2778436c110ae03589706cfc0d10a8bc84525e
Lazytangent <pm123633@gmail.com>
2021-08-05 19:20:49 -0500

    Update null_ls config

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -10,10 +10,6 @@ local sources = {
   null_ls.builtins.formatting.trim_whitespace.with({
     filetypes = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
   }),
-
-  null_ls.builtins.diagnostics.eslint.with({
-    command = "eslint_d"
-  }),
 }
 
 null_ls.config({ sources = sources, save_after_format = false, diagnostics_format = "[#{c}] #{m} (#{s})" })
