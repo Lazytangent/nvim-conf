2d321de41bf9267303f5eefd2efabf147e32d60b
Peter Mai <pm123633@gmail.com>
2021-08-09 19:19:09 -0500

    Update telescope settings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,5 @@
-local u = require("utils.core")
-
-vim.g.mapleader = ","
-vim.g.maplocalleader = " "
-
--- u.map("n", "<localleader>cc", "<cmd>lua require('telescope.builtin').colorscheme()<cr>")
-
--- u.map("n", "<localleader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
--- u.map("n", "<localleader>fc", "<cmd>lua require('telescope.builtin').commands()<cr>")
--- u.map("n", "<localleader>fd", "<cmd>lua require('telescope.builtin').lsp_document_diagnostics()<cr>")
--- u.map("n", "<localleader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
--- u.map("n", "<localleader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
--- u.map("n", "<localleader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
--- u.map("n", "<localleader>fl", "<cmd>lua require('telescope.builtin').loclist()<cr>")
--- u.map("n", "<localleader>fp", "<cmd>lua require('telescope.builtin').planets()<cr>")
--- u.map("n", "<localleader>fq", "<cmd>lua require('telescope.builtin').quickfix()<cr>")
--- u.map("n", "<localleader>fr", "<cmd>lua require('telescope.builtin').registers()<cr>")
--- u.map("n", "<localleader>ft", "<cmd>lua require('telescope.builtin').treesitter()<cr>")
-
--- u.map("n", "<leader>fb", "<cmd>lua require('telescope.builtin').file_browser()<cr>")
--- u.map("n", "<leader>fd", "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>")
--- u.map("n", "<leader>ff", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>")
--- u.map("n", "<leader>fg", "<cmd>lua require('telescope.builtin').git_status()<cr>")
--- u.map("n", "<leader>fr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>")
--- u.map("n", "<leader>ft", "<cmd>lua require('telescope.builtin').tags()<cr>")
+require('telescope').setup {
+	defaults = {
+		layout_strategy = "center",
+	}
+}
