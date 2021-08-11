8ceb3c5671f367b3f90158762f967d99775a89cb
Peter Mai <pm123633@gmail.com>
2021-08-10 19:28:32 -0500

    Update settings

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -3,7 +3,9 @@ local null_ls = require('null-ls')
 local sources = {
   null_ls.builtins.formatting.eslint_d,
   null_ls.builtins.formatting.json_tool,
-  null_ls.builtins.formatting.stylua,
+  null_ls.builtins.formatting.stylua.with({
+	  args = { "--search-parent-directories" },
+  }),
   null_ls.builtins.formatting.rustfmt,
   null_ls.builtins.formatting.sqlformat,
   null_ls.builtins.formatting.yapf,
