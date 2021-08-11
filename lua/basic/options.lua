7cc2099620f42f36617011a1b09a44fbce449240
Peter Mai <pm123633@gmail.com>
2021-08-11 08:53:20 -0500

    Remove undodir options

--- lua/basic/options.lua
+++ lua/basic/options.lua
@@ -60,7 +60,7 @@ u.opt("b", "swapfile", Opts.swapfile)
 u.opt("g", "guifont", Opts.guifont)
 u.opt("g", "grepprg", Opts.grepprg)
 u.opt("g", "shiftround", Opts.shiftround)
-u.opt("g", "undodir", Opts.undodir)
+-- u.opt("g", "undodir", Opts.undodir)
 u.opt("g", "undofile", Opts.undofile)
 
 -- Commands
