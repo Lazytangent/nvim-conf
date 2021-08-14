6461389c9714746fc3fc5f30544237b4c34c1bdd
Peter Mai <pm123633@gmail.com>
2021-08-14 10:10:11 -0500

    Update JS snippets

--- lua/plugins/snippets/javascript.lua
+++ lua/plugins/snippets/javascript.lua
@@ -16,89 +16,96 @@ local dl = require("luasnip.extras").dynamic_lambda
 local parse = require("luasnip").parser.parse_snippet
 
 local function copy(args)
-	return args[1]
+  return args[1]
 end
 
 local function dynamic_copy(args, old_state)
-	if not old_state then
-		old_state = {}
-	end
+  if not old_state then
+    old_state = {}
+  end
 
-	local node = i(1, args[1])
-	local snip = sn(nil, node)
-	snip.old_state = old_state
-	return snip
+  local node = i(1, args[1])
+  local snip = sn(nil, node)
+  snip.old_state = old_state
+  return snip
 end
 
 local javascript = {
-	s({ trig = "ctx", dscr = "Create Context" }, {
-		t("const "),
-		i(1, "NameOfContext"),
-		t({ " = createContext();", "export const " }),
-		i(2, "useContextHook"),
-		t(" = () => useContext("),
-		f(copy, 1),
-		t({ ");", "", "const " }),
-		i(3, "ContextProvider"),
-		t({ " = ({ children }) => {", "\t" }),
-		i(0),
-		t({ "", "\treturn (", "\t\t<" }),
-		f(copy, 1),
-		t(".Provider value={{"),
-		i(4),
-		t({ "}}>", "\t\t\t{children}", "\t\t</" }),
-		f(copy, 1),
-		t({ ".Provider>", "\t);", "};", "", "export default " }),
-		f(copy, 3),
-		t(";"),
-	}),
-	s({ trig = "fnc", dscr = "Functional Component" }, {
-		t("const "),
-		i(1, "Component"),
-		t(" = ("),
-		c(2, {
-			i(nil, "props"),
-			sn(nil, {
-				t("{ "),
-				i(1, "props"),
-				t(" }"),
-			}),
-		}),
-		t({ ") => {", "\t" }),
-		i(0),
-		t({ "", "\treturn (", "\t\t<h2>" }),
-		f(copy, 1),
-		t({ "works!</h2>", "\t);", "};", "", "export default " }),
-		f(copy, 1),
-		t(";"),
-	}),
-	s({ trig = "res.json", dscr = "Parse the JSON body of a response" }, {
-		t("const "),
-		c(1, {
-			i(nil, "name"),
-			sn(nil, {
-				t("{ "),
-				i(1, "name"),
-				t(" }"),
-			}),
-		}),
-		t(" = await res.json();"),
-	}),
-	s({ trig = "response.json", dscr = "Parse the JSON body of a response" }, {
-		t("const "),
-		c(1, {
-			i(nil, "name"),
-			sn(nil, {
-				t("{ "),
-				i(1, "name"),
-				t(" }"),
-			}),
-		}),
-		t(" = await response.json();"),
-	}),
+  s({ trig = "ctx", dscr = "Create Context" }, {
+    t("const "),
+    i(1, "NameOfContext"),
+    t({ " = createContext();", "export const " }),
+    i(2, "useContextHook"),
+    t(" = () => useContext("),
+    f(copy, 1),
+    t({ ");", "", "const " }),
+    i(3, "ContextProvider"),
+    t({ " = ({ children }) => {", "\t" }),
+    i(0),
+    t({ "", "\treturn (", "\t\t<" }),
+    f(copy, 1),
+    t(".Provider value={{"),
+    i(4),
+    t({ "}}>", "\t\t\t{children}", "\t\t</" }),
+    f(copy, 1),
+    t({ ".Provider>", "\t);", "};", "", "export default " }),
+    f(copy, 3),
+    t(";"),
+  }),
+  s({ trig = "fnc", dscr = "Functional Component" }, {
+    t("const "),
+    i(1, "Component"),
+    t(" = ("),
+    c(2, {
+      i(nil, "props"),
+      sn(nil, {
+        t("{ "),
+        i(1, "props"),
+        t(" }"),
+      }),
+    }),
+    t({ ") => {", "\t" }),
+    i(0),
+    t({ "", "\treturn (", "\t\t<h2>" }),
+    f(copy, 1),
+    t({ "works!</h2>", "\t);", "};", "", "export default " }),
+    f(copy, 1),
+    t(";"),
+  }),
+  s({ trig = "res.json", dscr = "Parse the JSON body of a response" }, {
+    t("const "),
+    c(1, {
+      i(nil, "name"),
+      sn(nil, {
+        t("{ "),
+        i(1, "name"),
+        t(" }"),
+      }),
+    }),
+    t(" = await res.json();"),
+  }),
+  s({ trig = "response.json", dscr = "Parse the JSON body of a response" }, {
+    t("const "),
+    c(1, {
+      i(nil, "name"),
+      sn(nil, {
+        t("{ "),
+        i(1, "name"),
+        t(" }"),
+      }),
+    }),
+    t(" = await response.json();"),
+  }),
+  s({ trig = "cr", dscr = "const _____ = require('_____');" }, {
+    t("const "),
+    i(2, "name"),
+    t(" = require('"),
+    i(1, "module"),
+    t(");"),
+  }),
 }
 
-local redux = require('plugins.snippets.redux')
+local redux = require("plugins.snippets.redux")
 vim.list_extend(javascript, redux)
 
 return javascript
