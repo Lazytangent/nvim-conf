620ccabfc7bb4ae251bd200a476643855034454a
Peter Mai <pm123633@gmail.com>
2021-08-22 16:16:00 -0500

    Configuring gps

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,5 @@
+local gps = require('nvim-gps')
+
 require("lualine").setup {
   options = {
     -- theme = "nightfly",
@@ -5,7 +7,7 @@ require("lualine").setup {
     icons_enabled = false,
   },
   sections = {
-    lualine_c = {{ 'filename', file_status = true, path = 1 }, 'diff'},
+    lualine_c = {{ 'filename', file_status = true, path = 1 }, 'diff', { gps.get_location, condition = gps.is_available }},
     lualine_z = {'location', {'diagnostics', sources = {'nvim_lsp'}, sections = {'error', 'warn', 'info'}}},
   },
 }
