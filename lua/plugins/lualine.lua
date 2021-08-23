68f7c7dd85fefdbf1a96725c87e42cb923801d4d
Peter Mai <pm123633@gmail.com>
2021-08-23 15:15:20 -0500

    Update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,6 +6,7 @@ require("lualine").setup {
     theme = 'tokyonight',
     icons_enabled = false,
     disabled_filetypes = {'NvimTree'},
+    extensions = { 'quickfix', 'fugitive' }
   },
   sections = {
     lualine_c = {{ 'filename', file_status = true, path = 1 }, 'diff', { gps.get_location, condition = gps.is_available }},
