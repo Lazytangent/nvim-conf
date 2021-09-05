180af824b1b993dd23b8f7f58d0b1fe9c4185779
Peter Mai <pm123633@gmail.com>
2021-09-05 11:59:28 -0500

    Add mapping for running Java code

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,3 +1,5 @@
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.makeprg = "javac %"
+
+vim.api.nvim_buf_set_keymap(0, 'n', '<localleader><leader>r', ':!java -cp %:p:h %:t:r<cr>', { noremap = true })
