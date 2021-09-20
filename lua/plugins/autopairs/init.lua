9dbf5e320b5d2ba9b5d426a39040d5078ae5aa71
Peter Mai <pm123633@gmail.com>
2021-09-20 15:56:42 -0500

    Add arrow function to autopairs for ts and js

--- lua/plugins/autopairs/init.lua
+++ lua/plugins/autopairs/init.lua
@@ -1,7 +1,14 @@
 local npairs = require('nvim-autopairs')
+local Rule = require('nvim-autopairs.rule')
 
 npairs.setup {}
 
 npairs.add_rules(require('nvim-autopairs.rules.endwise-elixir'))
 npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
 npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
+
+npairs.add_rules {
+  Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' })
+    :use_regex(true)
+    :set_end_pair_length(2),
+}
