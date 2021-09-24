a0325f884ae7e7ad951a67c0af3f3a30e9e6d690
Peter Mai <pm123633@gmail.com>
2021-09-24 07:20:35 -0500

    Update cmp settings to work with nvim-autopairs

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -34,7 +34,7 @@ cmp.setup {
 require("nvim-autopairs.completion.cmp").setup {
   map_cr = true,
   map_complete = true,
-  auto_select = true,
+  auto_select = false,
   insert = false,
   map_char = {
     all = '(',
