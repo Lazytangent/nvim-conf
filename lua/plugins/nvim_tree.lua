f0d7bf0555e57028d4facd0e98b68be52d46cdc1
Peter Mai <pm123633@gmail.com>
2021-10-03 15:47:25 -0500

    Update nvim-tree settings

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -1,9 +1,11 @@
--- Nvim Tree
-vim.g.nvim_tree_side = 'right'
 vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', '*.pyc'}
 vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_auto_open = 0
-vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_disable_window_picker = 1
--- vim.g.nvim_tree_disable_netrw = 0
+
+require('nvim-tree').setup {
+  auto_close = true,
+  view = {
+    side = 'right',
+  },
+}
