f6b33c1a507b3e6021a3f6e5e2ee314c2ccdacc5
Peter Mai <pm123633@gmail.com>
2021-11-09 18:09:35 -0600

    Update null_ls settings

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,16 +1,43 @@
 local null_ls = require("null-ls")
 
 local sources = {
+  -- Formatting
   null_ls.builtins.formatting.prettier,
   null_ls.builtins.formatting.eslint_d,
+
   null_ls.builtins.formatting.json_tool,
+
   null_ls.builtins.formatting.stylua,
+
   null_ls.builtins.formatting.rustfmt,
+
   null_ls.builtins.formatting.sqlformat,
+
   null_ls.builtins.formatting.yapf,
+  null_ls.builtins.formatting.autopep8,
+  null_ls.builtins.formatting.black,
+  null_ls.builtins.formatting.isort,
+
+  null_ls.builtins.formatting.rubocop,
+
+  null_ls.builtins.formatting.stylelint,
+
+  null_ls.builtins.formatting.gofmt,
+  null_ls.builtins.formatting.goimports,
+
   null_ls.builtins.formatting.trim_whitespace.with({
     filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
   }),
+
+  -- Linting
+  null_ls.builtins.diagnostics.flake8,
+  null_ls.builtins.diagnostics.pylint,
+
+  null_ls.builtins.diagnostics.rubocop,
+
+  null_ls.builtins.diagnostics.shellcheck,
+
+  null_ls.builtins.diagnostics.stylelint,
 }
 
 null_ls.config({ sources = sources, save_after_format = false, diagnostics_format = "[#{c}] #{m} (#{s})" })
