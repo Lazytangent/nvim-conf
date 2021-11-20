3ec148a967a5b644b3dd86361f1f8781ab4fd683
Peter Mai <pm123633@gmail.com>
2021-11-19 21:10:30 -0600

    Configure some orgmode mappings to match Doom"

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -2,4 +2,10 @@ require('orgmode').setup {
   org_agenda_files = {'~/Documents/org/*'},
   org_default_notes_file = '~/Documents/org/default.org',
   org_indent_mode = 'noindent',
+  mappings = {
+    org = {
+      org_todo = "<localleader>mtt",
+      org_todo_prev = "<localleader>mtd",
+    },
+  },
 }
