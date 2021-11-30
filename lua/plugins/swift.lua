9d142520510f113e57096918b033231627f883c6
Peter Mai <pm123633@gmail.com>
2021-11-30 17:03:27 -0600

    Add swift nvim plugin config

--- lua/plugins/swift.lua
+++ lua/plugins/swift.lua
@@ -1,1 +1,32 @@
-require('swift_env').setup {}
+require('swift_env').setup {
+  format = {
+    auto = false,
+    config_default_content = [[
+# see avaliable rules: https://github.com/nicklockwood/SwiftFormat/blob/master/Rules.md
+
+# Format Options
+--empty void
+--swiftversion 5.4
+--indent 2
+--indentcase true
+--semicolons inline
+--shortoptionals always
+--stripunusedargs always
+--wrapcollections before-first
+--wraparguments before-first
+--header "\n {file}\n\n Copyright (c) {year} Lazytangent\n\n Created by Lazytangent on {created}.\n"
+--tabwidth 2
+--self remove
+
+# Rules
+
+--disable linebreakAtEndOfFile
+--disable preferKeyPath
+--enable redundantParens
+--enable redundantLet
+--enable redundantNilInit
+--disable redundantPattern
+--enable redundantReturn
+    ]],
+  }
+}
