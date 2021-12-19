4cec37b2bac79049e99c12579037971ed7ced3aa
Lazytangent <pm123633@gmail.com>
2021-12-18 20:12:42 -0600

    Update dap install config

--- lua/plugins/dap_install/init.lua
+++ lua/plugins/dap_install/init.lua
@@ -2,6 +2,7 @@ local dap_install = require("dap-install")
 
 dap_install.setup({
   installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
+  verbosely_call_debuggers = true,
 })
 
 local dbg_list = require("dap-install.api.debuggers").get_installed_debuggers()
