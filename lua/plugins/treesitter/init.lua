dfb703c5c788782fbbf611c7180fac26fc507813
Lazytangent <peter.tn.mai@gmail.com>
2022-01-21 12:08:06 -0600

    Remove deprecated textobject

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -54,14 +54,6 @@ require("nvim-treesitter.configs").setup({
 
         ["ir"] = "@parameter.inner",
         ["ar"] = "@parameter.outer",
-
-        -- Or you can define your own textobjects like this
-        ["iF"] = {
-          python = "(function_definition) @function",
-          cpp = "(function_definition) @function",
-          c = "(function_definition) @function",
-          java = "(method_declaration) @function",
-        },
       },
     },
     move = {
