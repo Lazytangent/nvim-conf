d05f8f0143f58865b5b245e3bf0c8487bffc4d62
Lazytangent <peter.tn.mai@gmail.com>
2022-02-17 19:59:58 -0600

    Remove context highlighting from refactor

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -22,7 +22,6 @@ require("nvim-treesitter.configs").setup({
   },
   refactor = {
     highlight_definitions = { enable = true },
-    highlight_current_scope = { enable = true },
     navigation = {
       enable = true,
       keymaps = {
