e833dfa515579376d9deed6a489f13b517298adf
Lazytangent <peter.tn.mai@gmail.com>
2022-04-18 11:42:00 -0500

    Temporarily remove vim-matchup until treesitter fix

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -116,7 +116,7 @@ packer.startup(function()
 
   use({
     { "lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim" },
-    "andymass/vim-matchup",
+    -- "andymass/vim-matchup",
     "ggandor/lightspeed.nvim",
     "lukas-reineke/indent-blankline.nvim",
     "kyazdani42/nvim-web-devicons",
