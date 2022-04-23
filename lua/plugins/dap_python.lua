1244f7f7c80016f4965d549abdb4e5753b9595d1
Lazytangent <peter.tn.mai@gmail.com>
2022-04-23 17:34:30 -0500

    Add dap-python

---
+++ lua/plugins/dap_python.lua
@@ -0,0 +1,20 @@
+local dap_python = require('dap-python')
+local dap = require('dap')
+
+dap_python.setup('~/Documents/Debugger/debugpy/bin/python')
+dap_python.test_runner = 'pytest'
+
+table.insert(dap.configurations.python, {
+  name = "Flask: Local",
+  type = "python",
+  request = "launch",
+  module = "flask",
+  args = {"run", "--no-debugger", "--no-reload"},
+})
+
+table.insert(dap.configurations.python, {
+  name = 'Generic Remote',
+  type = 'Docker',
+  request = 'launch',
+
+})
