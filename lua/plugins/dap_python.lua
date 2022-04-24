91ca15abd3157e06eb9e0786c5194b0b0d4b81ae
Lazytangent <peter.tn.mai@gmail.com>
2022-04-24 18:53:18 -0500

    Connect via custom dap module

--- lua/plugins/dap_python.lua
+++ lua/plugins/dap_python.lua
@@ -3,26 +3,3 @@ local dap = require('dap')
 
 dap_python.setup('~/Documents/Debugger/debugpy/bin/python')
 dap_python.test_runner = 'pytest'
-
-table.insert(dap.configurations.python, {
-  name = "Flask: Local",
-  type = "python",
-  request = "launch",
-  module = "flask",
-  args = {"run", "--no-debugger", "--no-reload"},
-})
-
-table.insert(dap.configurations.python, {
-  name = 'Generic Remote',
-  type = 'Docker',
-  request = 'launch',
-
-})
-
-table.insert(dap.configurations.python, {
-  name = 'FastAPI',
-  type = 'python',
-  request = 'launch',
-  module = 'uvicorn',
-  args = {'main:app'}
-})
