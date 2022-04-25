4210190f39a82504f9607a6732fc3935a107066b
Lazytangent <peter.tn.mai@gmail.com>
2022-04-25 18:27:21 -0500

    Update comment settings

--- lua/plugins/comment_nvim.lua
+++ lua/plugins/comment_nvim.lua
@@ -8,7 +8,19 @@ require('Comment').setup {
   pre_hook = function(ctx)
     local u = require('Comment.utils')
     if ctx.ctype == u.ctype.line or ctx.cmotion == u.cmotion.line then
-      return require('ts_context_commentstring.internal').calculate_commentstring()
+      local U = require 'Comment.utils'
+
+      local location = nil
+      if ctx.ctype == U.ctype.block then
+        location = require('ts_context_commentstring.utils').get_cursor_location()
+      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+        location = require('ts_context_commentstring.utils').get_visual_start_location()
+      end
+
+      return require('ts_context_commentstring.internal').calculate_commentstring {
+        key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
+        location = location,
+      }
     end
   end
 }
--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -34,6 +34,16 @@ require("nvim-treesitter.configs").setup({
   },
   context_commentstring = {
     enable = true,
+    enable_autocmd = false,
+    config = {
+      javascript = {
+        __default = '// %s',
+        jsx_element = '{/* %s */}',
+        jsx_fragment = '{/* %s */}',
+        jsx_attribute = '// %s'
+        comment = '// %s',
+      },
+    },
   },
   textobjects = {
     select = {
