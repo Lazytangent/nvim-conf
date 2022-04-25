da84c51d98d235988640143c90b00c06e7423112
Lazytangent <peter.tn.mai@gmail.com>
2022-04-25 10:48:56 -0500

    Use new autocmd api

--- lua/basic/autocmds.lua
+++ lua/basic/autocmds.lua
@@ -1,38 +1,40 @@
-local u = require("utils.core")
+-- Highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    require('vim.highlight').on_yank()
+  end,
+})
 
-local autocmds = {
-  hl_yank = {
-    {"TextYankPost", "*", 'lua require"vim.highlight".on_yank()'},
-  },
-  preserve_cursor = {
-    {"BufReadPost", "*", [[if line("'\"") > 1 && line("'\"") <= line("$") && &ft !~# 'gitcommit' | exe "normal! g'\"" | endif]]},
-  },
-  trim_whitespaces = {
-    {"BufWritePre", "*", [[%s/\s\+$//e]]},
-    {"BufWritePre", "*", [[%s/\n\+\%$//e]]},
-    -- {"BufWritePre", "*.[ch]", [[*.[ch] %s/\%$/\r/e]]},
-  },
-  terminal = {
-    {"TermOpen", "*", [[setlocal nonumber norelativenumber bufhidden="delete"]]},
-    {"TermOpen", "*", "startinsert"},
-    -- {"BufWinEnter,WinEnter,BufRead", "term://*", "startinsert"},
-  },
-}
+-- Remember cursor
+vim.api.nvim_create_autocmd('BufReadPost', {
+  command = [[if line("'\"") > 1 && line("'\"") <= line("$") && &ft !~# 'gitcommit' | exe "normal! g'\"" | endif]],
+})
 
-u.define_augroups(autocmds)
+-- Remove trailing whitespace
+vim.api.nvim_create_autocmd('BufWritePre', {
+  command = [[%s/\s\+$//e]],
+})
 
-vim.cmd("autocmd FocusGained,BufEnter * checktime")
-vim.cmd([[ autocmd StdinReadPre * let s:std_in=1 ]])
--- autocmd VimEnter * if argc() == 0 && !exists('s:std_in') | NERDTree | endif
--- vim.cmd([[
---   autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
--- ]])
--- vim.cmd([[
---   autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 | let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
--- ]])
--- vim.cmd("highlight ColorColumn ctermbg=magenta")
--- vim.cmd("call matchadd('ColorColumn', '%81v', 100)")
--- vim.fn.matchadd('ColorColumn', '%81v', 100)
--- gitcommit = {
-  -- {"FileType", "gitcommit", [[setlocal spell]]}
--- }
+-- Remove trailing whitespace
+vim.api.nvim_create_autocmd('BufWritePre', {
+  command = [[%s/\n\+\%$//e]],
+})
+
+-- Set terminal buffer defaults
+vim.api.nvim_create_autocmd('TermOpen', {
+  command = [[setlocal nonumber norelativenumber bufhidden="delete"]],
+})
+
+-- Set terminal buffer defaults
+vim.api.nvim_create_autocmd('TermOpen', {
+  command = "startinsert",
+})
+
+-- Update file if file has been updated outside of buffer
+vim.api.nvim_create_autocmd({'FocusGained', 'BufEnter'}, {
+  command = "checktime",
+})
+
+vim.api.nvim_create_autocmd('StdinReadPre', {
+  command = [[let s:std_in=1]],
+})
