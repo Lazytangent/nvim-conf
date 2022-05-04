46c5b8e4e39c598b6f6d1f44bf35a99f465ed9c4
Lazytangent <peter.tn.mai@gmail.com>
2022-05-04 10:17:21 -0500

    Add change directory mapping for find_files

--- lua/plugins/telescope/config.lua
+++ lua/plugins/telescope/config.lua
@@ -7,7 +7,21 @@ require("telescope").setup({
       }
     },
   }),
+  pickers = {
+    find_files = {
+      mappings = {
+        n = {
+          ["cd"] = function(prompt_bufnr)
+            local selection = require("telescope.actions.state").get_selected_entry()
+            local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+            require("telescope.actions").close(prompt_bufnr)
+            vim.cmd(string.format("silent cd %s", dir))
+          end
+        },
+      },
     },
+  },
+  extensions = {
     ["ui-select"] = {
       require("telescope.themes").get_dropdown {}
     },
