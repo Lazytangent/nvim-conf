d0112ca1cbfbf65cc78a86fa8dc9f4f8a0761e99
Lazytangent <peter.tn.mai@gmail.com>
2022-05-05 10:07:54 -0500

    Clean up lsp config

--- lua/lsp/regular/on_attach.lua
+++ lua/lsp/regular/on_attach.lua
@@ -6,7 +6,7 @@ local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, ...)
   end
 
-  -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings
   local opts = { noremap = true, silent = true }
@@ -30,33 +30,6 @@ local on_attach = function(client, bufnr)
   buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
   buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
   buf_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<cr>", opts)
-
-  -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
-    buf_set_option("formatexpr", "v:lua.vim.lsp.formatexpr()")
-    buf_set_keymap("n", "<space>,f", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
-  end
-  if client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap("v", "<space>,f", "<cmd>lua vim.lsp.buf.range_formatting()<cr>", opts)
-  end
-
-  if client.resolved_capabilities.goto_definition then
-    buf_set_option("tagfunc", "v:lua.vim.lsp.tagfunc()")
-  end
-
-  -- Set autocommands conditional on server capabilities
-  -- if client.resolved_capabilities.document_highlight then
-  --   vim.api.nvim_exec([[
-  --     highlight LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-  --     highlight LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-  --     highlight LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-  --     augroup lsp_document_highlight
-  --       autocmd! * <buffer>
-  --       autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-  --       autocmd CursorHold <buffer> lua vim.lsp.buf.clear_references()
-  --     augroup END
-  --   ]], false)
-  -- end
 end
 
 return on_attach
