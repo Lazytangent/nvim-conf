943ef62344d80658ed0ad562197eecae6754b673
Lazytangent <peter.tn.mai@gmail.com>
2022-05-05 10:10:47 -0500

    Split out <leader>f mappings

--- lua/mappings/normal/leader/init.lua
+++ lua/mappings/normal/leader/init.lua
@@ -1,4 +1,5 @@
 local d = require('mappings.normal.leader.d')
+local f = require('mappings.normal.leader.f')
 
 local leader = {
   ["<leader>"] = {
@@ -87,43 +88,7 @@ local leader = {
   },
   d = d,
   e = "Show line diagnostics",
-  f = {
-    name = "+Telescope",
-    a = { [[<cmd>lua require('telescope').extensions.frecency.frecency()<cr>]], "Frecency" },
-    b = { [[<cmd>lua require('telescope.builtin').buffers()<cr>]], "Find Buffers" },
-    c = { [[<cmd>lua require('telescope.builtin').commands()<cr>]], "Commands" },
-    d = { [[<cmd>lua require('telescope.builtin').diagnostics()<cr>]], "LSP Diagnostics" },
-    e = { [[<cmd>lua require('telescope.builtin').grep_string()<cr>]], "Search for string under cursor" },
-    f = { [[<cmd>lua require('telescope.builtin').find_files()<cr>]], "Find files" },
-    g = { [[<cmd>lua require('telescope').extensions.live_grep_raw.live_grep_raw()<cr>]], "Live grep" },
-    h = { [[<cmd>lua require('telescope.builtin').help_tags()<cr>]], "Help tags" },
-    i = { [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>]], "In buffer" },
-    j = { [[<cmd>lua require('telescope.builtin').jumplist()<cr>]], "Jumplist" },
-    k = { [[<cmd>lua require('telescope.builtin').lsp_references()<cr>]], "LSP References" },
-    l = { [[<cmd>lua require('telescope.builtin').loclist()<cr>]], "Location list" },
-    m = { [[<cmd>lua require('telescope.builtin').marks()<cr>]], "Marks" },
-    n = { [[<cmd>lua require('telescope.builtin').diagnostics({ bufnr = 0 })<cr>]], "LSP Diagnostics in buffer" },
-    o = { [[<cmd>lua require('telescope.builtin').quickfixhistory()<cr>]], "Old Quickfix Lists" },
-    p = { [[<cmd>lua require('telescope.builtin').pickers()<cr>]], "Prev Pickers" },
-    q = { [[<cmd>lua require('telescope.builtin').quickfix()<cr>]], "Quickfix list" },
-    r = { [[<cmd>lua require('telescope.builtin').registers()<cr>]], "Registers" },
-    t = { [[<cmd>lua require('telescope.builtin').treesitter()<cr>]], "Treesitter" },
-
-    z = {
-      name = "+LSP Specific",
-      d = { [[<cmd>lua require('telescope.builtin').lsp_definitions()<cr>]], "LSP Definitions" },
-      i = { [[<cmd>lua require('telescope.builtin').lsp_implementations()<cr>]], "LSP Implementations" },
-      r = { [[<cmd>lua require('telescope.builtin').lsp_references()<cr>]], "LSP References" },
-    },
-
-    A = { [[<cmd>lua require('plugins.telescope').search_all_files()<cr>]], "All files" },
-    B = { [[<cmd>lua require('telescope').extensions.file_browser.file_browser()<cr>]], "File browser" },
-    D = { [[<cmd>lua require('telescope.builtin').lsp_definitions()<cr>]], "LSP Definitions" },
-    I = { [[<cmd>lua require('telescope.builtin').lsp_implementations()<cr>]], "LSP Implementations" },
-    R = { [[<cmd>lua require('telescope.builtin').lsp_references()<cr>]], "LSP References" },
-
-    s = { [[<cmd>w<cr>]], "Save file" },
-  },
+  f = f,
   g = {
     name = "+Git",
     c = { [[<cmd>Git commit<cr>]], "commit" },
