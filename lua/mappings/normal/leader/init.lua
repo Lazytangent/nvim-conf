3589fdcd19dfb64305a297756578342614c87dbc
Lazytangent <peter.tn.mai@gmail.com>
2022-05-04 21:07:42 -0500

    Update mappings

--- lua/mappings/normal/leader/init.lua
+++ lua/mappings/normal/leader/init.lua
@@ -92,7 +92,7 @@ local leader = {
     a = { [[<cmd>lua require('telescope').extensions.frecency.frecency()<cr>]], "Frecency" },
     b = { [[<cmd>lua require('telescope.builtin').buffers()<cr>]], "Find Buffers" },
     c = { [[<cmd>lua require('telescope.builtin').commands()<cr>]], "Commands" },
-    d = { [[<cmd>lua require('telescope').extensions.file_browser.file_browser()<cr>]], "File browser" },
+    d = { [[<cmd>lua require('telescope.builtin').diagnostics()<cr>]], "LSP Diagnostics" },
     e = { [[<cmd>lua require('telescope.builtin').grep_string()<cr>]], "Search for string under cursor" },
     f = { [[<cmd>lua require('telescope.builtin').find_files()<cr>]], "Find files" },
     g = { [[<cmd>lua require('telescope').extensions.live_grep_raw.live_grep_raw()<cr>]], "Live grep" },
@@ -102,9 +102,11 @@ local leader = {
     k = { [[<cmd>lua require('telescope.builtin').lsp_references()<cr>]], "LSP References" },
     l = { [[<cmd>lua require('telescope.builtin').loclist()<cr>]], "Location list" },
     m = { [[<cmd>lua require('telescope.builtin').marks()<cr>]], "Marks" },
-    n = { [[<cmd>lua require('telescope.builtin').diagnostics(0)<cr>]], "LSP Diagnostics" },
+    n = { [[<cmd>lua require('telescope.builtin').diagnostics({ bufnr = 0 })<cr>]], "LSP Diagnostics in buffer" },
+    o = { [[<cmd>lua require('telescope.builtin').quickfixhistory()<cr>]], "Old Quickfix Lists" },
     p = { [[<cmd>lua require('telescope.builtin').pickers()<cr>]], "Prev Pickers" },
     q = { [[<cmd>lua require('telescope.builtin').quickfix()<cr>]], "Quickfix list" },
+    r = { [[<cmd>lua require('telescope.builtin').registers()<cr>]], "Registers" },
     t = { [[<cmd>lua require('telescope.builtin').treesitter()<cr>]], "Treesitter" },
 
     z = {
@@ -113,6 +115,9 @@ local leader = {
       i = { [[<cmd>lua require('telescope.builtin').lsp_implementations()<cr>]], "LSP Implementations" },
       r = { [[<cmd>lua require('telescope.builtin').lsp_references()<cr>]], "LSP References" },
     },
+
+    A = { [[<cmd>lua require('plugins.telescope').search_all_files()<cr>]], "All files" },
+    B = { [[<cmd>lua require('telescope').extensions.file_browser.file_browser()<cr>]], "File browser" },
 
     s = { [[<cmd>w<cr>]], "Save file" },
   },
