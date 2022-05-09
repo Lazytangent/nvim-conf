1f9a51cf073c1c66bc0bad6c80b99fc3fcba9449
Lazytangent <peter.tn.mai@gmail.com>
2022-05-09 17:00:57 -0500

    Add bufnr

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -5,4 +5,4 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
 vim.opt_local.textwidth = 120
 
-vim.api.nvim_buf_set_keymap('n', '<localleader><leader>r', ':!python %<cr>', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, 'n', '<localleader><leader>r', ':!python %<cr>', { noremap = true })
