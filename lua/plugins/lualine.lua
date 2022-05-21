73ade7001d549ce1bce9408906e0aaa89f1ae377
Lazytangent <peter.tn.mai@gmail.com>
2022-05-21 13:46:52 -0500

    Formatting

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,16 +2,30 @@ local gps = require('nvim-gps')
 
 require("lualine").setup {
   options = {
-    -- theme = "nightfly",
-    theme = 'tokyonight',
-    -- theme = 'catppuccin',
+    theme = 'auto',
     icons_enabled = false,
     disabled_filetypes = {'NvimTree'},
     extensions = { 'quickfix', 'fugitive' },
   },
   sections = {
-    lualine_b = {'branch', 'diff',
-                  {'diagnostics', sources = { 'nvim_diagnostic' }}},
-    lualine_c = { "os.date('%c')", { 'filename', file_status = true, path = 1 }, 'diff', { gps.get_location, cond = gps.is_available } },
+    lualine_b = {
+      'branch',
+      'diff',
+      {'diagnostics', sources = { 'nvim_diagnostic', 'nvim_lsp' }}
+    },
+    lualine_c = {
+      "os.date('%c')",
+      { 'filename', file_status = true, path = 1 },
+      'diff',
+      { gps.get_location, cond = gps.is_available }
+    },
+  },
+  tabline = {
+    lualine_a = {
+      {
+        "windows",
+        mode = 2,
+      },
+    },
   },
 }
