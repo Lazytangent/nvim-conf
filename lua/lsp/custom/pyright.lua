8cbd95979d9d8189ca549fa70cd0365360ade1be
Lazytangent <peter.tn.mai@gmail.com>
2022-06-15 20:52:34 -0500

    Add capabilities to Pyright

--- lua/lsp/custom/pyright.lua
+++ lua/lsp/custom/pyright.lua
@@ -25,4 +25,5 @@ require('lspconfig').pyright.setup {
   on_init = function(client)
     client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
   end,
+  capabilities = capabilities,
 }
