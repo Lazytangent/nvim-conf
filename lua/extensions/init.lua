0a37f28918bf9246f9f028cce2fafaa826147e43
Lazytangent <peter.tn.mai@gmail.com>
2022-06-22 19:55:51 -0500

    Remove hydra

--- lua/extensions/init.lua
+++ lua/extensions/init.lua
@@ -105,8 +105,6 @@ packer.startup(function()
     "benfowler/telescope-luasnip.nvim",
   })
 
-  use({ 'anuvyklack/hydra.nvim', requires = "anuvyklack/keymap-layer.nvim" })
-
   use({
     { "lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim" },
     { "ldelossa/gh.nvim", requires = { "ldelossa/litee.nvim" } },
