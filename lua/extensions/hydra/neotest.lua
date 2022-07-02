ae53668d65c65395d5fb69438057c72b624e0884
Lazytangent <peter.tn.mai@gmail.com>
2022-07-02 15:32:32 -0500

    Update neotest hydra

--- lua/extensions/hydra/neotest.lua
+++ lua/extensions/hydra/neotest.lua
@@ -4,6 +4,8 @@ local utils = require('mappings.utils')
 local hint = [[
 _a_: Attach _d_: Debug nearest test _f_: Run file _o_: Open output _s_: Stop running test _t_: Run nearest test
 _mc_: Close summary _mo_: Open summary _mm_: Toggle summary _mt_: Toggle summary
+
+                        _n_: Next failed test _p_: Prev failed test
 ]]
 
 local call_func = function(module, func, args)
@@ -21,12 +23,14 @@ Hydra({
     color = 'pink',
     invoke_on_body = true,
   },
-  body = '<leader>r',
+  body = '<leader>n',
   heads = {
     { 'a', utils.lua_require('neotest', 'run.attach()') },
     { 'd', utils.lua_require('neotest', "run.run({ strategy = 'dap' })") },
     { 'f', utils.lua_require('neotest', "run.run(vim.fn.expand('%'))") },
+    { 'n', utils.lua_require('neotest', "jump.next({ status = 'failed' })") },
     { 'o', utils.lua_require('neotest', 'output.open({ enter = true })') },
+    { 'p', utils.lua_require('neotest', "jump.prev({ status = 'failed' })") },
     { 's', utils.lua_require('neotest', 'run.stop()') },
     { 't', utils.lua_require('neotest', 'run.run()') },
     { 'mc', utils.lua_require('neotest', 'summary.close()') },
