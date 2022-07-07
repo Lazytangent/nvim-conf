f9faf9fd48f5929774cc8d54b65f42df9c703bd7
Lazytangent <peter.tn.mai@gmail.com>
2022-07-07 17:05:09 -0500

    Remove unused code

--- lua/extensions/telescope/config.lua
+++ lua/extensions/telescope/config.lua
@@ -11,55 +11,6 @@ telescope.setup({
       }
     },
   }),
-  pickers = {
-    find_files = {
-      mappings = {
-        n = {
-          ["cd"] = function(prompt_bufnr)
-            local selection = require("telescope.actions.state").get_selected_entry()
-            local dir = vim.fn.fnamemodify(selection.path, ":p:h")
-            require("telescope.actions").close(prompt_bufnr)
-            vim.cmd(string.format("silent cd %s", dir))
-          end,
-          ["c"] = function(prompt_bufnr)
-            local cwd = vim.fn.getcwd()
-            local Path = require("plenary.path")
-            local os_sep = Path.path.sep
-            local default = cwd .. os_sep
-            local current_picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-            local finder = current_picker.finder
-            local fb_utils = require("telescope._extensions.file_browser.utils")
-
-            vim.ui.input({ prompt = "Insert the file name:\n", default = default }, function(file)
-              if not file then
-                return
-              end
-              if file == "" then
-                print "Please enter valid filename!"
-                return
-              end
-              if file == cwd .. os_sep then
-                print "Please enter valid file or folder name!"
-                return
-              end
-              file = Path:new(file)
-
-              if file:exists() then
-                error "File or folder already exists."
-                return
-              end
-              if not fb_utils.is_dir(file.filename) then
-                file:touch { parents = true }
-              else
-                Path:new(file.filename:sub(1, -2)):mkdir { parents = true }
-              end
-              current_picker:refresh(finder, { reset_prompt = true })
-            end)
-          end,
-        },
-      },
-    },
-  },
   extensions = {
     ["ui-select"] = {
       require("telescope.themes").get_dropdown {}
