4bd0fa42d7b2671b01ffdcbf489f29f873ac20c7
Lazytangent <peter.tn.mai@gmail.com>
2022-07-13 13:20:14 -0500

    Add load_modules utility function

--- lua/utils/core.lua
+++ lua/utils/core.lua
@@ -29,4 +29,10 @@ function utils.search_nvim()
   )
 end
 
+function utils.load_modules(base, modules)
+  for _, module in ipairs(modules) do
+    require(base .. "." .. module)
+  end
+end
+
 return utils
