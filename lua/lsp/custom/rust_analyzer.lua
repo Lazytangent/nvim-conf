edb1e04ea269be82a38faae88dcabbf25fe1a7d7
Lazytangent <peter.tn.mai@gmail.com>
2022-09-24 19:40:28 -0500

    Configure custom settings for rust-analyzer
    I'm now using rust-analyzer from the stable rustup toolchain instead of
    as a binary from Homebrew. This is mainly to just keep the Rust tools
    organized by either rustup or cargo.

---
+++ lua/lsp/custom/rust_analyzer.lua
@@ -0,0 +1,7 @@
+local lsp = require("lspconfig")
+local on_attach = require("lsp.primary.on_attach")
+local capabilities = require('lsp.capabilities')
+
+lsp.rust_analyzer.setup {
+  cmd = { "rustup", "run", "stable", "rust-analyzer" },
+}
