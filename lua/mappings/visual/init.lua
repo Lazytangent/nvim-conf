495ec3768f2b2ee702796565683ea5c692995f3c
Lazytangent <peter.tn.mai@gmail.com>
2022-10-13 12:13:39 -0500

    Add dial.nvim mappings

--- lua/mappings/visual/init.lua
+++ lua/mappings/visual/init.lua
@@ -1,8 +1,16 @@
 local leader = require('mappings.visual.leader')
+local utils = require("mappings.utils")
 
 local visual = {
+  g = {
+    ["<C-a>"] = { utils.lua_require("dial.map", "inc_gvisual()"), "Increment with dial" },
+    ["<C-x>"] = { utils.lua_require("dial.map", "dec_gvisual()"), "Decrement with dial" },
+  },
+
+  ["<C-a>"] = { utils.lua_require("dial.map", "inc_visual()"), "Increment with dial" },
   ["<C-k>"] = { [[:<C-U>exec "'<,           '>move '<-" . (1+v:count1)<cr>gv]], "Move line up" },
   ["<C-j>"] = { [[:<C-U>exec "'<,           '>move '>+" . (0+v:count1)<cr>gv]], "Move line down" },
+  ["<C-x>"] = { utils.lua_require("dial.map", "dec_visual()"), "Decrement with dial" },
   ["<S-h>"] = { [[<cmd>DVB_Drag('left')]],  "Drag left" },
   ["<S-l>"] = { [[<cmd>DVB_Drag('right')]], "Drag right" },
   ["<S-j>"] = { [[<cmd>DVB_Drag('down')]],  "Drag down" },
