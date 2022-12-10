f5427f537a5a602c415fe4a4fda57ae8acc641f7
Lazytangent <peter.tn.mai@gmail.com>
2022-12-10 16:23:51 -0600

    Switch vimtex to use Skim.app for PDF

--- lua/extensions/vimtex.lua
+++ lua/extensions/vimtex.lua
@@ -1,4 +1,6 @@
-vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_view_method = 'skim'
+vim.g.vimtex_view_skim_sync = 1
+vim.g.vimtex_view_skim_activate = 1
 vim.g.vimtex_quickfix_mode = 0
 -- vim.g.tex_conceal = 'abdmg'
 vim.g.vimtex_compiler_latexmk = {
