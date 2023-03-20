7c846126f46c53ae9ed5a49c7e609ddd409e4897
Lazytangent <peter.tn.mai@gmail.com>
2023-03-20 16:22:43 -0500

    Ignore kotlin for treesitter indentation

--- lua/extensions/treesitter/init.lua
+++ lua/extensions/treesitter/init.lua
@@ -5,7 +5,7 @@ require("nvim-treesitter.configs").setup({
 
   indent = {
     enable = false,
-    disable = { 'python' },
+    disable = { 'python', 'kotlin' },
   },
   matchup = { enable = true },
   rainbow = {
