93ec00186e3e3d45fc8bd7422b2317a0d8ff990a
Lazytangent <peter.tn.mai@gmail.com>
2023-03-23 10:43:54 -0500

    Update jdtls settings for arch-linux based system

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -15,18 +15,8 @@ local on_attach = require("lsp.primary.on_attach")
 -- vim/.config/nvim/ftplugin/java.lua
 local config = {
   cmd = {
-    'java',
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xms1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    '-jar', vim.fn.glob(home .. '/.local/bin/jdt-language-server/plugins/org.eclipse.equinox.launcher_*.jar'),
-    '-configuration', vim.fn.glob(home .. '/.local/bin/jdt-language-server/config_mac'),
+    'jdtls',
+    '-configuration', '/usr/share/java/jdtls/config_linux',
     '-data', workspace_folder
   },
   root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
