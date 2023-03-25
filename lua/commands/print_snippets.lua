847465b05c6ab32eac68a3efc712e4c7d6b893f8
Lazytangent <peter.tn.mai@gmail.com>
2023-03-25 11:36:13 -0500

    Print table out to new vsplit buffer

---
+++ lua/commands/print_snippets.lua
@@ -0,0 +1,56 @@
+local keys = {
+    "trigger",
+    --[[ "dscr", ]]
+    "id",
+    "name",
+    --[[ "docstring", ]]
+    --[[ "condition", ]]
+}
+
+local first_to_upper = function(str)
+    return (str:gsub("^%l", string.upper))
+end
+
+local create_line = function(keys)
+    local content = ""
+    for _, key in ipairs(keys) do
+        local piece = string.format("%-15s", key)
+        content = content .. "| " .. piece .. " "
+    end
+
+    return content
+end
+
+local create_headline = function(keys)
+    local titlecased_keys = {}
+    for _, key in ipairs(keys) do
+        table.insert(titlecased_keys, first_to_upper(key))
+    end
+
+    return create_line(titlecased_keys)
+end
+
+vim.api.nvim_create_user_command('DescribeSnippetsTable', function(args)
+  local filetype = vim.bo.filetype
+  local snippets = require('luasnip').get_snippets(filetype, { type = "snippets" })
+
+  local bufnr = vim.api.nvim_create_buf(true, true)
+
+  local headline = create_headline(keys)
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { headline })
+  local break_ = ("-"):rep(string.len(headline))
+  vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { break_ })
+
+  for _, snippet in ipairs(snippets) do
+      local snippet_table = {}
+      for _, key in ipairs(keys) do
+          table.insert(snippet_table, snippet[key])
+      end
+      local line = create_line(snippet_table)
+    vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { line })
+  end
+
+  vim.cmd.vsplit()
+  vim.api.nvim_win_set_buf(0, bufnr)
+
+end, {})
