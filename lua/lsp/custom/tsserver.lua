5751ad9af8d2af155f9dd3c66cd4f816d818d436
Lazytangent <peter.tn.mai@gmail.com>
2023-03-30 11:51:18 -0500

    Add inlay hints support for ts and js

---
+++ lua/lsp/custom/tsserver.lua
@@ -0,0 +1,33 @@
+local lsp = require('lspconfig')
+local on_attach = require('lsp.primary.on_attach')
+local capabilities = require('lsp.capabilities')
+
+lsp.tsserver.setup {
+  settings = {
+    typescript = {
+      inlayHints = {
+        includeInlayParameterNameHints = 'all',
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+    javascript = {
+      inlayHints = {
+        includeInlayParameterNameHints = 'all',
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+  },
+
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
