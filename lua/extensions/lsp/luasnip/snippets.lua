f68a59a362a14618c1175ca09cef735957f2cdda
Lazytangent <peter.tn.mai@gmail.com>
2023-04-11 08:51:31 -0500

    Use built-in snippet loader for org snippets

--- lua/extensions/lsp/luasnip/snippets.lua
+++ lua/extensions/lsp/luasnip/snippets.lua
@@ -13,7 +13,6 @@ local function bash(_, command)
 end
 
 local javascript = require("extensions.snippets.javascript")
-local org        = require("extensions.snippets.orgmode")
 local terraform  = require("extensions.snippets.terraform")
 local tex        = require("extensions.snippets.tex")
 local vue        = require("extensions.snippets.vue")
@@ -24,7 +23,6 @@ local all = {
 
 ls.add_snippets('all', all)
 ls.add_snippets('javascript', javascript)
-ls.add_snippets('org', org)
 ls.add_snippets('terraform', terraform)
 ls.add_snippets('tex', tex)
 ls.add_snippets('typescript', javascript)
@@ -34,6 +32,7 @@ ls.add_snippets('vue', vue)
 require("luasnip.loaders.from_vscode").load({ paths = "~/.config/nvim/lua/snippets" })
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load()
+require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets" })
 
 ls.filetype_extend("ruby", {"rails"})
 ls.filetype_extend("html", {"twig"})
