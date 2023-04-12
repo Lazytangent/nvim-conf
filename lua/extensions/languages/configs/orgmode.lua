35be354f958bbeac3edf5c86d4dbcc7bf41caacf
Lazytangent <peter.tn.mai@gmail.com>
2023-04-12 12:13:51 -0500

    Update orgmode settings

--- lua/extensions/languages/configs/orgmode.lua
+++ lua/extensions/languages/configs/orgmode.lua
@@ -3,9 +3,14 @@ local orgmode = require('orgmode')
 orgmode.setup_ts_grammar()
 
 orgmode.setup {
-  org_agenda_files = {'~/Documents/org/*'},
-  org_default_notes_file = '~/Documents/org/default.org',
+  org_agenda_files = {'~/Documents/org/*', '~/.local/src/org/*'},
+  org_default_notes_file = '~/.local/src/org/default.org',
   org_indent_mode = 'noindent',
+  org_hide_emphasis_markers = true,
+  org_highlight_latex_and_related = "entities",
+  emacs_config = {
+    config_path = "$HOME/.config/emacs",
+  },
   mappings = {
     org = {
       org_todo = "<localleader>mtt",
