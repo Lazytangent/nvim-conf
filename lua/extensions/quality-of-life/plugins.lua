e0cc56c75fa5de78ced6b3080c5b4cbf87bdf0ea
Peter Mai <peter.tn.mai@gmail.com>
2023-08-01 09:24:26 -0500

    Update flash.nvim settings

--- lua/extensions/quality-of-life/plugins.lua
+++ lua/extensions/quality-of-life/plugins.lua
@@ -74,6 +74,10 @@ return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
+    config = true,
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    },
   },
   {
     "andymass/vim-matchup",
