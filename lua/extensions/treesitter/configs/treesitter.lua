e9d6f3d0f4b64ffab236df6ddfd7daa7513c4f67
Peter Mai <peter.tn.mai@gmail.com>
2023-09-05 18:52:55 -0500

    Add condition for disabled tree-sitter when file is too large

--- lua/extensions/treesitter/configs/treesitter.lua
+++ lua/extensions/treesitter/configs/treesitter.lua
@@ -18,6 +18,12 @@ require("nvim-treesitter.configs").setup({
         end
       end
 
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+
       if vim.api.nvim_buf_line_count(bufnr) > 50000 then
         return true
       end
