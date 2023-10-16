fb908a39264d5d51e0d3540b9d1c98b8786a5892
Peter Mai <peter.tn.mai@gmail.com>
2023-10-16 14:29:59 -0500

    Cleanup

--- lua/lsp/regular/on_attach.lua
+++ lua/lsp/regular/on_attach.lua
@@ -1,26 +1,24 @@
-local utils = require("mappings.utils")
-
 local on_attach = function(client, bufnr)
   local opts = { noremap = true, silent = true }
-  vim.keymap.set("n", "gd", utils.lua_cmd("vim.lsp.buf.definition()"), opts)
-  vim.keymap.set("n", "gD", utils.lua_cmd("vim.lsp.buf.declaration()"), opts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
-  vim.keymap.set("n", "<C-h>",            utils.lua_cmd("vim.lsp.buf.signature_help()"), opts)
-  vim.keymap.set("n", "<localleader>pwa", utils.lua_cmd("vim.lsp.buf.add_workspace_folder()"), opts)
-  vim.keymap.set("n", "<localleader>pwr", utils.lua_cmd("vim.lsp.buf.remove_workspace_folder()"), opts)
-  vim.keymap.set("n", "<localleader>pwl", utils.lua_cmd("print(vim.inspect(vim.lsp.buf.list_workspace_folders()))"), opts)
-  vim.keymap.set("n", "<localleader>pt",  utils.lua_cmd("vim.lsp.buf.type_definition()"), opts)
-  vim.keymap.set("n", "<localleader>pc",  utils.lua_cmd("vim.lsp.buf.code_action()"), opts)
-  vim.keymap.set("n", "<localleader>pr",  utils.lua_cmd("vim.lsp.buf.rename()"), opts)
-  vim.keymap.set("n", "<localleader>r",   utils.lua_cmd("vim.lsp.buf.rename()"), opts)
+  vim.keymap.set("n", "<C-h>",            vim.lsp.buf.signature_help, opts)
+  vim.keymap.set("n", "<localleader>pwa", vim.lsp.buf.add_workspace_folder, opts)
+  vim.keymap.set("n", "<localleader>pwr", vim.lsp.buf.remove_workspace_folder, opts)
+  vim.keymap.set("n", "<localleader>pwl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+  vim.keymap.set("n", "<localleader>pt",  vim.lsp.buf.type_definition, opts)
+  vim.keymap.set("n", "<localleader>pc",  vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<localleader>pr",  vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<localleader>r",   vim.lsp.buf.rename, opts)
   vim.keymap.set("n", "<space>,f",        function() require("conform").format({ async = true }) end, opts)
 
-  vim.keymap.set("n", "<leader>e", utils.lua_cmd("vim.diagnostic.open_float()"), opts)
-  vim.keymap.set("n", "[d",        utils.lua_cmd("vim.diagnostic.goto_prev()"), opts)
-  vim.keymap.set("n", "]d",        utils.lua_cmd("vim.diagnostic.goto_next()"), opts)
-  vim.keymap.set("n", "[e",        utils.lua_cmd("vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })"), opts)
-  vim.keymap.set("n", "]e",        utils.lua_cmd("vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })"), opts)
-  vim.keymap.set("n", "<space>q",  utils.lua_cmd("vim.diagnostic.setloclist()"), opts)
+  vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "[d",        vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d",        vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "[e",        function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end, opts)
+  vim.keymap.set("n", "]e",        function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end, opts)
+  vim.keymap.set("n", "<space>q",  vim.diagnostic.setloclist, opts)
 end
 
 return on_attach
