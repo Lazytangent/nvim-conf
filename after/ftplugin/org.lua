62542a3b767d8b6e5a44e0892b026dd36cf72490
Peter Mai <peter.tn.mai@gmail.com>
2023-11-07 10:55:49 -0600

    Remove code that was erroring

--- after/ftplugin/org.lua
+++ after/ftplugin/org.lua
@@ -1,11 +1,3 @@
 vim.opt_local.formatoptions:append('r')
 vim.opt_local.comments:remove("fb:-")
 vim.opt_local.comments:append(":-")
-
-local wk = require("which-key")
-
-wk.register({
-  ["<localleader>"] = {
-    n = { "/#+<cr>i<cr><Esc><cmd>nohlsearch<cr>", "Fix code block" },
-  },
-}, { buffer = 0 })
