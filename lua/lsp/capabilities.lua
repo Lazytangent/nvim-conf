2708be5e8f555488b4f61479794112d5ed92723c
Peter Mai <peter.tn.mai@gmail.com>
2023-11-16 14:56:28 -0600

    Update lsp capabilities per suggestion

--- lua/lsp/capabilities.lua
+++ lua/lsp/capabilities.lua
@@ -1,1 +1,6 @@
-return require('cmp_nvim_lsp').default_capabilities()
+local capabilities = vim.tbl_deep_extend("force",
+  vim.lsp.protocol.make_client_capabilities(),
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+return capabilities
