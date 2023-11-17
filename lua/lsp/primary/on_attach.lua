c99deba233e90a7a56df8ac1ff7f01dbd87c7f72
Peter Mai <peter.tn.mai@gmail.com>
2023-11-17 14:57:33 -0600

    Turn off inlay_hint

--- lua/lsp/primary/on_attach.lua
+++ lua/lsp/primary/on_attach.lua
@@ -1,6 +1,6 @@
 local on_attach = function(client, bufnr)
   if client.server_capabilities.inlayHintProvider then
-    vim.lsp.inlay_hint.enable(bufnr, true)
+    vim.lsp.inlay_hint.enable(bufnr, false)
   end
 
   if client.server_capabilities.documentSymbols then
