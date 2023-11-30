520c058eeacb8409ac5b3cda595431183f595ffc
Peter Mai <peter.tn.mai@gmail.com>
2023-11-30 16:18:51 -0600

    Set softtabstop and shiftwidth for js and ts files

--- ftplugin/javascript.lua
+++ ftplugin/javascript.lua
@@ -1,7 +1,7 @@
 vim.opt_local.suffixesadd:append({ '.js', '.jsx', '.css' })
 vim.opt_local.foldenable = true
 vim.opt_local.cindent = false
-vim.opt_local.softtabstop = 2
-vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
 
 vim.api.nvim_buf_set_keymap(0, 'n', '<localleader><leader>r', ':!node %<cr>', { noremap = true })
