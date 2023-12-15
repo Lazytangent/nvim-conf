5e5b8cb3e2fec530d7687363b472db7920f911e2
Peter Mai <peter.tn.mai@gmail.com>
2023-12-15 11:26:57 -0600

    Slight performance optimizations?

--- lua/extensions/languages/plugins.lua
+++ lua/extensions/languages/plugins.lua
@@ -78,6 +78,44 @@ return {
     },
     event = "VeryLazy",
   },
+  -- {
+  --   "mrcjkb/haskell-tools.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim",
+  --   },
+  --   branch = "1.x.x",
+  --   event = "VeryLazy",
+  -- },
+  {
+    "sealemar/vtl",
+    event = "VeryLazy",
+  },
+  {
+    "tpope/vim-cucumber",
+    event = "VeryLazy",
+  },
+  {
+    "alaviss/nim.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "akinsho/flutter-tools.nvim",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim',
+    },
+    opts = {
+      lsp = {
+        on_attach = require('lsp.primary.on_attach'),
+      },
+    },
+    event = "VeryLazy",
+  },
+  {
+    "dart-lang/dart-vim-plugin",
+    event = "VeryLazy",
+  },
   {
     "aklt/plantuml-syntax",
     event = "VeryLazy",
