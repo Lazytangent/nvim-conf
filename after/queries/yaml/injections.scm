50ccc3a71df33e9f26a1162547f7604b144c69ff
Peter Mai <peter.tn.mai@gmail.com>
2023-12-31 15:13:34 -0600

    Yaml injections

--- after/queries/yaml/injections.scm
+++ after/queries/yaml/injections.scm
@@ -1,4 +1,9 @@
 (block_mapping_pair
  key: (flow_node) @_type (#lua-match? @_type "script")
  value: (block_node
-         (block_scalar) @javascript (#offset! @javascript 0 0 0 0)))
+         (block_scalar) @injection.content (#set! injection.language "javascript")))
+
+(block_node
+  (block_scalar
+    (comment) @injection.language (#offset! @injection.language 0 3 0 0))
+  @injection.content (#offset! @injection.content 1 0 0 0))
--- queries/yaml/injections.scm
+++ queries/yaml/injections.scm
@@ -1,24 +1,6 @@
-(block_mapping_pair
-  key: (flow_node) @_run (#any-of? @_run "script")
-  value: (flow_node
-           (plain_scalar) @javascript))
+; extends
 
-(block_mapping_pair
-  key: (flow_node) @_run (#any-of? @_run "script")
-  value: (block_node
-           (block_scalar) @javascript (#offset! @javascript 0 0 0 0)))
-
-(block_mapping_pair
-  key: (flow_node) @_run (#any-of? @_run "script")
-  value: (block_node
-           (block_sequence
-             (block_sequence_item
-               (flow_node) @javascript))))
-
-(block_mapping_pair
-  key: (flow_node) @_run (#any-of? @_run "script")
-  value: (block_node
-           (block_sequence
-             (block_sequence_item
-               (block_node
-                  (block_scalar) @javascript)))))
+(block_node
+  (block_scalar
+    (comment) @injection.language (#offset! @injection.language 0 2 0 0))
+  @injection.content (#offset! @injection.content 0 1 0 0))
