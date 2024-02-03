944bca5823ac451a41b7ddb235c9950cb6e27279
Peter Mai <peter.tn.mai@gmail.com>
2024-02-03 15:31:43 -0600

    More orgmode config changes

--- lua/extensions/languages/configs/orgmode.lua
+++ lua/extensions/languages/configs/orgmode.lua
@@ -3,13 +3,15 @@ local orgmode = require('orgmode')
 orgmode.setup_ts_grammar()
 
 orgmode.setup {
+  org_adapt_indentation = false,
   org_agenda_files = {'~/Documents/org/*', '~/.local/src/org/*'},
+  org_agenda_skip_deadline_if_done = true,
+  org_agenda_skip_scheduled_if_done = true,
   org_default_notes_file = '~/.local/src/org/default.org',
   org_hide_emphasis_markers = true,
   org_highlight_latex_and_related = "entities",
   org_src_window_setup = "botright 16new",
-  org_agenda_skip_scheduled_if_done = true,
-  org_agenda_skip_deadline_if_done = true,
+  org_startup_folded = 'showeverything',
   emacs_config = {
     config_path = "$HOME/.config/emacs/init.el",
   },
