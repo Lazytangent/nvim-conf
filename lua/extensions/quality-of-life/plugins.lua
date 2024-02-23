7e9f71affd1e65453708c547628cf12e1245f069
Peter Mai <peter.tn.mai@gmail.com>
2024-02-23 13:51:18 -0600

    Potentially add copilot

--- lua/extensions/quality-of-life/plugins.lua
+++ lua/extensions/quality-of-life/plugins.lua
@@ -259,4 +259,18 @@ return {
       },
     },
   },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup()
+  --   end,
+  -- },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   config = function()
+  --     require("copilot_cmp").setup()
+  --   end,
+  -- },
 }
