589e4e44fe1ebe61d2feca51f3fae0a8454708a3
Peter Mai <peter.tn.mai@gmail.com>
2024-02-28 16:19:43 -0600

    Don't set indentation for work file types

--- ftplugin/typescriptreact.lua
+++ ftplugin/typescriptreact.lua
@@ -2,3 +2,5 @@ vim.opt_local.suffixesadd:append({ '.js', '.jsx', '.css' })
 vim.opt_local.foldenable = true
 vim.opt_local.cindent = false
 vim.opt_local.spell = false
+-- vim.opt_local.softtabstop = 2
+-- vim.opt_local.shiftwidth = 2
