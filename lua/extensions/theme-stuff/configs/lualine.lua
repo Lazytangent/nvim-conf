3f6b5344f6995b4a7cb116d7c736beaa0ce21e76
Peter Mai <peter.tn.mai@gmail.com>
2024-02-28 16:20:10 -0600

    Add indent component to lualine

--- lua/extensions/theme-stuff/configs/lualine.lua
+++ lua/extensions/theme-stuff/configs/lualine.lua
@@ -1,5 +1,12 @@
 local jsonpath = require('jsonpath')
 
+local function indent()
+  local softtabstop = vim.bo.softtabstop
+  local shiftwidth = vim.bo.shiftwidth
+
+  return "sts=" .. softtabstop .. ", sw=" .. shiftwidth
+end
+
 require("lualine").setup {
   extensions = { 'quickfix', 'fugitive' },
   options = {
@@ -27,6 +34,12 @@ require("lualine").setup {
       "os.date('%c')",
       { 'filename', file_status = true },
       'diff',
+    },
+    lualine_x = {
+      indent,
+      'encoding',
+      'fileformat',
+      'filetype',
     },
   },
   winbar = {
