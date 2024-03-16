8c568d4fbf177ea71cc1012b71235cd6192f53a6
Peter Mai <peter.tn.mai@gmail.com>
2024-03-16 18:43:53 -0500

    Update rust injections

--- queries/rust/injections.scm
+++ queries/rust/injections.scm
@@ -1,3 +1,8 @@
+;; extends
+
 (macro_invocation
   ((identifier) @_name (#eq? @_name "html"))
   ((token_tree) @html (#offset! @html 0 1 0 -1)))
+
+(((block_comment) @injection.language (#offset! @injection.language 0 3 0 -3))
+ ((raw_string_literal) @injection.content))
