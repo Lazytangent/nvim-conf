d86fe622a48427ceae5b59fccc0e45d27d754510
Peter Mai <peter.tn.mai@gmail.com>
2024-03-31 19:43:43 -0500

    More java setup

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,20 +2,34 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.makeprg = "javac %"
 
-vim.api.nvim_buf_set_keymap(0, 'n', '<localleader><leader>r', ':!java -cp %:p:h %:t:r<cr>', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, 'n', '<localleader><leader>r', ':!/usr/lib/jvm/java-22-openjdk/bin/java -cp %:p:h %:t:r<cr>', { noremap = true })
 
 local jdtls = require('jdtls')
 local root_markers = {'gradlew', '.git'}
 local root_dir = require('jdtls.setup').find_root(root_markers)
 local home = os.getenv("HOME")
-local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+local workspace_folder = home .. "/.local/share/eclipse-workspace/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 local on_attach = require("lsp.primary.on_attach")
 
 -- See mfussenegger/dotfiles for more inspiration
 -- vim/.config/nvim/ftplugin/java.lua
+vim.fn.setenv('JAVA_HOME', '/usr/lib/jvm/java-22-openjdk')
+
 local config = {
   cmd = {
     '/usr/bin/jdtls',
+
+    -- '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    -- '-Dosgi.bundles.defaultStartLevel=4',
+    -- '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    -- '-Dlog.protocol=true',
+    -- '-Dlog.level=ALL',
+    -- '-Xmx1g',
+    -- '--add-modules=ALL-SYSTEM',
+    -- '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    -- '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    -- '-jar', '/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher.gtk.linux.x86_64_1.2.900.v20240129-1338.jar',
     '-configuration', '/usr/share/java/jdtls/config_linux',
     '-data', workspace_folder
   },
