378ae1fba6f5a3f4d239cadba7edd71b9510d12e
Peter Mai <peter.tn.mai@gmail.com>
2024-04-02 18:47:18 -0500

    Maybe working?

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,7 +2,7 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.makeprg = "javac %"
 
-vim.api.nvim_buf_set_keymap(0, 'n', '<localleader><leader>r', ':!/usr/lib/jvm/java-22-jdk/bin/java -cp %:p:h %:t:r<cr>', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, 'n', '<localleader><leader>r', ':!/usr/lib/jvm/java-17-jdk/bin/java -cp %:p:h %:t:r<cr>', { noremap = true })
 
 local jdtls = require('jdtls')
 local root_markers = {'gradlew', '.git'}
@@ -13,58 +13,52 @@ local on_attach = require("lsp.primary.on_attach")
 
 -- See mfussenegger/dotfiles for more inspiration
 -- vim/.config/nvim/ftplugin/java.lua
--- vim.fn.setenv('JAVA_HOME', '/usr/lib/jvm/java-17-openjdk/')
-vim.fn.setenv('JAVA_HOME', '/usr/lib/jvm/java-22-jdk/')
 
 local bundles = {
   home .. '/.local/src/programming-stuff/language-servers/lsp4jakarta/jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/target/org.eclipse.lsp4jakarta.jdt.core-0.2.2-SNAPSHOT.jar',
 }
 
--- for _, bundle in ipairs(vim.split(vim.fn.glob('/usr/lib/eclipse/plugins/*.jar'), '\n')) do
---   table.insert(bundles, bundle)
--- end
+for _, bundle in ipairs(vim.split(vim.fn.glob('/usr/lib/eclipse/plugins/*.jar'), '\n')) do
+  table.insert(bundles, bundle)
+end
 
--- for _, bundle in ipairs(vim.split(vim.fn.glob(home .. '/.local/src/apache-tomcat-7.0.109/lib/*.jar'), '\n')) do
---   table.insert(bundles, bundle)
--- end
+for _, bundle in ipairs(vim.split(vim.fn.glob(home .. '/.local/src/apache-tomcat-7.0.109/lib/*.jar'), '\n')) do
+  table.insert(bundles, bundle)
+end
+
+local jar = vim.fn.glob(home .. '/.local/src/repos/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/plugins/org.eclipse.equinox.launcher_*.jar', true)
+local configuration = home .. '/.local/src/repos/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/config_linux'
 
 local config = {
   cmd = {
-    -- '/usr/lib/jvm/java-22-jdk/bin/java',
-    -- '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    -- '-Dosgi.bundles.defaultStartLevel=4',
-    -- '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    -- '-Dlog.protocol=true',
-    -- '-Dlog.level=ALL',
-    -- '-Xmx1g',
-    -- '--add-modules=ALL-SYSTEM',
-    -- '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    -- '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    -- '-jar', vim.fn.glob('/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
-
-    '/usr/bin/jdtls',
-    '--jvm-arg=-Dlog.level=ALL',
-    '--jvm-arg=-Dlog.protocol=true',
-    '--jvm-arg=-Xmx1g',
-
-    '-configuration', '/usr/share/java/jdtls/config_linux',
+    '/usr/lib/jvm/java-17-openjdk/bin/java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+    '-jar', jar,
+    '-configuration', configuration,
     '-data', workspace_folder,
   },
   root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
   settings = {
     java = {
-      -- inlayHints = {
-      --   parameterNames = {
-      --     enabled = "all",
-      --   },
-      -- },
+      inlayHints = {
+        parameterNames = {
+          enabled = "all",
+        },
+      },
       configuration = {
         runtimes = {
           {
             name = "JavaSE-22",
             path = "/usr/lib/jvm/java-22-jdk/",
           },
-
           {
             name = "JavaSE-17",
             path = "/usr/lib/jvm/java-17-openjdk/",
@@ -74,11 +68,11 @@ local config = {
       eclipse = {
         downloadSources = true,
       },
-      -- project = {
-      --   referencedLibraries = {
-          -- home .. '/.local/src/apache-tomcat-7.0.109/lib/servlet-api.jar',
-      --   },
-      -- },
+      project = {
+        referencedLibraries = {
+          home .. '/.local/src/apache-tomcat-7.0.109/lib/servlet-api.jar',
+        },
+      },
     },
   },
   init_options = {
