c2d22b0f04106e7b4064b0af95b970fd2750ca05
Peter Mai <peter.tn.mai@gmail.com>
2024-04-04 15:23:14 -0500

    Add typescript support to conform.nvim config

--- lua/extensions/quality-of-life/plugins.lua
+++ lua/extensions/quality-of-life/plugins.lua
@@ -261,6 +261,7 @@ return {
         rust = { "rustfmt" },
         sql = { "sql_formatter" },
         terraform = { "terraform_fmt" },
+        typescript = { "prettier", "eslint_d" },
         ["*"] = { "trim_whitespace" },
       },
     },
