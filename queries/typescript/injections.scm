d0acfd83d97a3d7d76395a6156caf750392aa8b1
Peter Mai <peter.tn.mai@gmail.com>
2024-04-10 20:13:40 -0500

    Updates

--- queries/typescript/injections.scm
+++ queries/typescript/injections.scm
@@ -1,6 +1,9 @@
-; inherits: javascript
 ; extends
 
 (((comment) @injection.language (#offset! @injection.language 0 3 0 -3))
   (template_string
+    (string_fragment) @injection.content))
+
+(((comment) @injection.language (#offset! @injection.language 0 3 0 -3))
+  (string
     (string_fragment) @injection.content))
