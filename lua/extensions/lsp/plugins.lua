1a8d8c30c156a674818203220717cf8244eb420c
Peter Mai <peter.tn.mai@gmail.com>
2024-05-21 07:56:13 -0500

    Disable plugin

--- lua/extensions/lsp/plugins.lua
+++ lua/extensions/lsp/plugins.lua
@@ -25,7 +25,7 @@ return {
       "hrsh7th/cmp-calc",
       "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lsp-document-symbol",
+      -- "hrsh7th/cmp-nvim-lsp-document-symbol",
       "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-omni",
       "hrsh7th/cmp-path",
