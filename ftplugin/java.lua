663b581e99ceac48b142781817206019a40bbf4b
Peter Mai <peter.tn.mai@gmail.com>
2024-07-08 17:18:18 -0500

    Remove print statement for debugging

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -9,7 +9,6 @@ local root_markers = {'gradlew', '.git'}
 local root_dir = require('jdtls.setup').find_root(root_markers)
 local home = os.getenv("HOME")
 local workspace_folder = home .. "/.local/share/jdtls-workspaces/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
-print(workspace_folder)
 local on_attach = require("lsp.primary.on_attach")
 
 -- See mfussenegger/dotfiles for more inspiration
