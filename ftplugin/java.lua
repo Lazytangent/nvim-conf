c643e44938a66ac162efc7d270e7b2bbee8d85a9
Peter Mai <peter.tn.mai@gmail.com>
2024-08-16 11:43:07 -0500

    Not using more java stuff

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,11 +2,12 @@ vim.opt_local.makeprg = "javac %"
 
 vim.api.nvim_buf_set_keymap(0, 'n', '<localleader><leader>r', ':!/usr/lib/jvm/java-17-jdk/bin/java -cp %:p:h %:t:r<cr>', { noremap = true })
 
+-- local root_markers = {'gradlew', '.git'}
+-- local root_dir = require('jdtls.setup').find_root(root_markers)
+-- local workspace_folder = home .. "/.local/share/jdtls-workspaces/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+
 local jdtls = require('jdtls')
-local root_markers = {'gradlew', '.git'}
-local root_dir = require('jdtls.setup').find_root(root_markers)
-local home = os.getenv("HOME")
-local workspace_folder = home .. "/.local/share/jdtls-workspaces/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+-- local home = os.getenv("HOME")
 local on_attach = require("lsp.primary.on_attach")
 
 -- See mfussenegger/dotfiles for more inspiration
@@ -14,8 +15,8 @@ local on_attach = require("lsp.primary.on_attach")
 
 -- Java LS extensions
 local bundles = {
-  home .. '/.local/src/programming-stuff/language-servers/lsp4jakarta/jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/target/org.eclipse.lsp4jakarta.jdt.core-0.2.2-SNAPSHOT.jar',
-  vim.fn.glob(home .. '/.local/src/dev/vscode-pde/extension/server/*.jar'),
+  -- home .. '/.local/src/programming-stuff/language-servers/lsp4jakarta/jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/target/org.eclipse.lsp4jakarta.jdt.core-0.2.2-SNAPSHOT.jar',
+  -- vim.fn.glob(home .. '/.local/src/dev/vscode-pde/extension/server/*.jar'),
 }
 
 -- local jar = vim.fn.glob(home .. '/.local/src/repos/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository/plugins/org.eclipse.equinox.launcher_*.jar', true)
