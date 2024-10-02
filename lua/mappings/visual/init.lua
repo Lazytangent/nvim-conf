4a0cb28f6e8d7c84a39120ef97f75fd2091bdd21
Peter Mai <peter.tn.mai@gmail.com>
2024-10-02 08:40:37 -0500

    Fix dial mappings

--- lua/mappings/visual/init.lua
+++ lua/mappings/visual/init.lua
@@ -1,19 +1,43 @@
-local leader = require('mappings.visual.leader')
+local dial_map = require "dial.map"
 
 local visual = {
-  { "g<C-a>", require("dial.map").inc_gvisual(), desc = "Increment with dial" },
-  { "<gC-x>", require("dial.map").dec_gvisual(), desc = "Decrement with dial" },
+  {
+    "g<C-a>",
+    function()
+      dial_map.manipulate("increment", "gvisual")
+    end,
+    desc = "Increment with dial"
+  },
+  {
+    "g<C-x>",
+    function()
+      dial_map.manipulate("decrement", "gvisual")
+    end,
+    desc = "Decrement with dial"
+  },
 
-  { "<C-a>", require("dial.map").inc_visual(), desc = "Increment with dial" },
-  { "<C-k>", [[:<C-U>exec "'<,           '>move '<-" . (1+v:count1)<cr>gv]], desc = "Move line up" },
-  { "<C-j>", [[:<C-U>exec "'<,           '>move '>+" . (0+v:count1)<cr>gv]], desc = "Move line down" },
-  { "<C-x>", require("dial.map").dec_visual(), desc = "Decrement with dial" },
+  {
+    "<C-a>",
+    function()
+      dial_map.manipulate("increment", "visual")
+    end,
+    desc = "Increment with dial",
+  },
+  {
+    "<C-x>",
+    function()
+      dial_map.manipulate("decrement", "visual")
+    end,
+    desc = "Decrement with dial"
+  },
+
+  { "<C-k>", [[:<C-U>exec "'<,'>move '<-" . (1+v:count1)<cr>gv]], desc = "Move line up" },
+  { "<C-j>", [[:<C-U>exec "'<,'>move '>+" . (0+v:count1)<cr>gv]], desc = "Move line down" },
+
   { "<S-h>", [[<cmd>DVB_Drag('left')]], desc = "Drag left" },
   { "<S-l>", [[<cmd>DVB_Drag('right')]], desc = "Drag right" },
   { "<S-j>", [[<cmd>DVB_Drag('down')]], desc = "Drag down" },
   { "<S-k>", [[<cmd>DVB_Drag('up')]],  desc = "Drag up" },
-
-  leader,
 }
 
 return visual
