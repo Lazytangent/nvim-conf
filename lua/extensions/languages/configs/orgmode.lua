5d366ac2c35f9071715c0947dc4dd06b573f36e4
Peter Mai <peter.tn.mai@gmail.com>
2024-10-08 17:26:41 -0500

    Update org src window size

--- lua/extensions/languages/configs/orgmode.lua
+++ lua/extensions/languages/configs/orgmode.lua
@@ -8,7 +8,7 @@ orgmode.setup {
   org_default_notes_file = '~/.local/src/org/default.org',
   org_hide_emphasis_markers = true,
   org_highlight_latex_and_related = "entities",
-  org_src_window_setup = "botright 16new",
+  org_src_window_setup = "botright new",
   org_startup_folded = 'showeverything',
   emacs_config = {
     config_path = "$HOME/.config/emacs/init.el",
