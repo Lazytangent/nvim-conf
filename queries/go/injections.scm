cc79ee26c2c641d67467aaab0c7ec43ba18cb36f
Peter Mai <peter.tn.mai@gmail.com>
2024-10-14 17:37:14 -0500

    More language injections for go

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -3,3 +3,6 @@
 (((comment) @injection.language (#offset! @injection.language 0 3 0 -3))
  ((expression_list
     (raw_string_literal) @injection.content)))
+
+(((comment) @injection.language (#offset! @injection.language 0 3 0 -3))
+ ((raw_string_literal) @injection.content (#offset! @injection.content 0 1 0 -1)))
