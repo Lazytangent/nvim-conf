42b385f284b7c21aeb542ed7c5e92ddb5dbc0cb5
Peter Mai <peter.tn.mai@gmail.com>
2024-10-27 13:07:57 -0500

    Update mappings

--- lua/mappings/normal/leader/x.lua
+++ lua/mappings/normal/leader/x.lua
@@ -1,7 +1,7 @@
 return {
   { "<leader>x",  group = "Trouble" },
-  { "<leader>xR", function() vim.cmd "Trouble lsp_references" end, desc = "toggle lsp references" },
   { "<leader>xl", function() vim.cmd "Trouble loclist" end,        desc = "toggle location list" },
   { "<leader>xq", function() vim.cmd "Trouble quickfix" end,       desc = "toggle quickfix" },
+  { "<leader>xr", function() vim.cmd "Trouble lsp_references" end, desc = "toggle lsp references" },
   { "<leader>xx", function() vim.cmd "Trouble diagnostics" end,    desc = "toggle" },
 }
