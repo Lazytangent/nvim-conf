5c9841229a1f9c7919f4e0858c77538e53ed0284
Peter Mai <peter.tn.mai@gmail.com>
2024-11-16 08:20:44 -0600

    Try flash.nvim autohide for char movement

--- lua/extensions/qol/folke.lua
+++ lua/extensions/qol/folke.lua
@@ -40,7 +40,13 @@ return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      modes = {
+        char = {
+          autohide = true,
+        },
+      },
+    },
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = "n", function() require("flash").treesitter() end, desc = "Flash Treesitter" },
