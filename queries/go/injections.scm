36d565275b1317cad1857ddc8c1ea3eb18823dbb
Peter Mai <peter.tn.mai@gmail.com>
2025-03-02 15:28:55 -0600

    Updated go language injections

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -9,3 +9,11 @@
 
 (((comment) @injection.language (#offset! @injection.language 0 3 0 -3))
  (expression_list (interpreted_string_literal) @injection.content (#offset! @injection.content 0 1 0 -1)))
+
+(((comment) @injection.language (#offset! @injection.language 0 3 0 -3))
+ ((interpreted_string_literal
+    (interpreted_string_literal_content) @injection.content)))
+
+(((comment) @injection.language (#offset! @injection.language 0 3 0 -3))
+ ((raw_string_literal
+    (raw_string_literal_content) @injection.content)))
