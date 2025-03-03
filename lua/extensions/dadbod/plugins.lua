ee61d0396f3298d9c45e3c30b22c17071bbf8692
Peter Mai <peter.tn.mai@gmail.com>
2025-03-03 17:00:43 -0600

    Update dbui config

--- lua/extensions/dadbod/plugins.lua
+++ lua/extensions/dadbod/plugins.lua
@@ -2,10 +2,6 @@ return {
   "tpope/vim-dadbod",
   {
     "kristijanhusak/vim-dadbod-ui",
-    init = function()
-      vim.g.db_ui_env_variable_url = 'DATABASE_URL'
-      vim.g.db_ui_env_variable_name = 'DATABASE_NAME'
-    end,
     cmd = "DBUI",
   },
   "kristijanhusak/vim-dadbod-completion",
