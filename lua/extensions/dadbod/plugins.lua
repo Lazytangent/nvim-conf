e7a734522ba05ee01dcf5443683f0f7d68c73754
Peter Mai <peter.tn.mai@gmail.com>
2025-03-03 17:06:20 -0600

    More DBUI config

--- lua/extensions/dadbod/plugins.lua
+++ lua/extensions/dadbod/plugins.lua
@@ -3,6 +3,10 @@ return {
   {
     "kristijanhusak/vim-dadbod-ui",
     cmd = "DBUI",
+    init = function()
+      vim.g.db_ui_tmp_query_location = '~/.local/share/queries'
+      vim.g.db_ui_winwidth = 60
+    end,
   },
   "kristijanhusak/vim-dadbod-completion",
 }
