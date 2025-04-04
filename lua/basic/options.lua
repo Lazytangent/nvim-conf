77215a7baa74e7c1ac81f91bafd08221dba22269
Peter Mai <peter.tn.mai@gmail.com>
2025-04-03 21:33:59 -0500

    Update vim.diagnostics settings

--- lua/basic/options.lua
+++ lua/basic/options.lua
@@ -80,11 +80,5 @@ vim.cmd("let g:python3_host_prog = '" .. python3_host_prog .. "'")
 vim.diagnostic.config({
   virtual_text = {
     current_line = true,
-    severity = {
-      max = vim.diagnostic.severity.WARN,
-    },
-  },
-  virtual_lines = {
-    current_line = true,
   },
 })
