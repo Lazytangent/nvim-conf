0ea0f54ecf9b455272e31a65f1dc4f9d17ae654d
Peter Mai <peter.tn.mai@gmail.com>
2025-05-26 16:20:48 -0500

    Add buffer-local nvim-surround setup for tex

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -11,3 +11,26 @@ for word, list in pairs(abbrevs) do
     vim.cmd("Abolish " .. shortcut .. " " .. word)
   end
 end
+
+require('nvim-surround').buffer_setup {
+  surrounds = {
+    ["c"] = {
+      add = function()
+        local cmd = require('nvim-surround.config').get_input "Command: "
+        return {
+          { "\\" .. cmd .. "{" },
+          { "}" },
+        }
+      end,
+    },
+    ["e"] = {
+      add = function()
+        local env = require('nvim-surround.config').get_input "Environment: "
+        return {
+          { "\\begin{" .. env .. "}" },
+          { "\\end{" .. env .. "}" },
+        }
+      end,
+    },
+  },
+}
