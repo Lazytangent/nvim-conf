44c0480dfc22daece54bebb5fb326165f109c72e
Peter Mai <peter.tn.mai@gmail.com>
2025-05-27 08:31:48 -0500

    Add custom hints for vimtex mappings

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -34,3 +34,36 @@ require('nvim-surround').buffer_setup {
     },
   },
 }
+
+local mappings = {
+  { '[m', desc = "[Count] Previous start of an environment begin" },
+  { ']m', desc = "[Count] Next start of an environment begin" },
+  { '[M', desc = "[Count] Previous end of an environment end" },
+  { ']M', desc = "[Count] Next end of an environment end" },
+
+  { ']]', desc = "[Count] Next beginning of a section" },
+  { '][', desc = "[Count] Next end of a section" },
+  { '[[', desc = '[Count] Previous beginning of a section' },
+  { '[]', desc = '[Count] Previous end of a section' },
+
+  { ']n', desc = '[Count] Next start of a math zone' },
+  { ']N', desc = '[Count] Next end of a math zone' },
+  { '[n', desc = '[Count] Previous start of a math zone' },
+  { '[N', desc = '[Count] Previous end of a math zone' },
+
+  { ']r', desc = '[Count] Next start of a frame environment' },
+  { ']R', desc = '[Count] Next end of a frame environment' },
+  { '[r', desc = '[Count] Previous start of a frame environment' },
+  { '[R', desc = '[Count] Previous end of a frame environment' },
+
+  { ']/', desc = '[Count] Next start of a LaTeX comment' },
+  { ']*', desc = '[Count] Next end of a LaTeX comment' },
+  { '[/', desc = '[Count] Previous start of a LaTeX comment' },
+  { '[*', desc = '[Count] Previous end of a LaTeX comment' },
+}
+
+require('which-key').add {
+  {
+    mappings
+  }
+}
