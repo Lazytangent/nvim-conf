2113332d94267da75b8883e76d13b07ba9a4c00e
Peter Mai <peter.tn.mai@gmail.com>
2025-06-04 08:56:21 -0500

    Add vim-sexp plugins

--- lua/extensions/languages/lisp.lua
+++ lua/extensions/languages/lisp.lua
@@ -1,7 +1,16 @@
 return {
-  "kovisoft/slimv",
-  init = function()
-    vim.g.slimv_indent_disable = true
-    vim.g.slimv_unmap_cr = true
-  end,
+  {
+    "kovisoft/slimv",
+    init = function()
+      vim.g.slimv_indent_disable = true
+      vim.g.slimv_unmap_cr = true
+    end,
+  },
+  {
+    "tpope/vim-sexp-mappings-for-regular-people",
+    dependencies = {
+      "guns/vim-sexp",
+      "tpope/vim-repeat",
+    },
+  },
 }
