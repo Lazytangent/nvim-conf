94098d589f0f860840294f5a91ace63facb6ff42
Peter Mai <peter.tn.mai@gmail.com>
2025-06-18 15:59:23 -0500

    Simplify ruff lsp

--- lua/lsp/custom/ruff.lua
+++ lua/lsp/custom/ruff.lua
@@ -1,17 +1,1 @@
-local lsp = require('lspconfig')
-local on_attach = require('lsp.primary.on_attach')
-local capabilities = require('lsp.capabilities')
-
-lsp.ruff.setup {
-  init_options = {
-    settings = {
-      args = {
-        -- TODO: Make this conditional? Or somehow configure a default line
-        -- length of 120 globally
-        "--ignore=E501",  -- Ignore line length complaints.
-      }
-    },
-  },
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
+vim.lsp.enable('ruff')
