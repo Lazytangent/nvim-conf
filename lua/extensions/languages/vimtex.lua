19f5048bd7d1b3160e80f04fd20a79287bfc9498
Peter Mai <peter.tn.mai@gmail.com>
2025-06-18 18:03:54 -0500

    Reverting changes from merge

--- lua/extensions/languages/vimtex.lua
+++ lua/extensions/languages/vimtex.lua
@@ -2,10 +2,10 @@ return {
   "lervag/vimtex",
   ft = "tex",
   config = function()
-    -- vim.g.vimtex_view_method = 'zathura'
-    vim.g.vimtex_view_method = 'skim'
-    vim.g.vimtex_view_skim_sync = 1
-    vim.g.vimtex_view_skim_activate = 1
+    vim.g.vimtex_view_method = 'zathura'
+    -- vim.g.vimtex_view_method = 'skim'
+    -- vim.g.vimtex_view_skim_sync = 1
+    -- vim.g.vimtex_view_skim_activate = 1
     vim.g.vimtex_quickfix_mode = 0
     -- vim.g.tex_conceal = 'abdmg'
     vim.g.vimtex_compiler_latexmk = {
