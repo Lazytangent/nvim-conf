d0664319e089cb0183df14a28dbc06b20e9144f7
Peter Mai <peter.tn.mai@gmail.com>
2025-06-29 17:18:13 -0500

    Extend lua function snippet with choice node

--- snippets/lua.lua
+++ snippets/lua.lua
@@ -1,13 +1,27 @@
 local snippets = {
   s({ trig = "func", name = "function", dscr = "Function" },
-    fmta([[
-    function <>(<>)
-      <>
-    end
-    ]], {
-      i(1, "name"),
-      i(2, "args"),
-      i(0, "body"),
+    c(1, {
+      fmta(
+        [[
+        function <>(<>)
+          <>
+        end
+        ]], { i(1, "name"), i(2, "args"), i(0, "body") }
+      ),
+      fmta(
+        [[
+        local function <> (<>)
+          <>
+        end
+        ]], { i(1, "name"), i(2, "args"), i(0, "body") }
+      ),
+      fmta(
+        [[
+        local <> = function (<>)
+          <>
+        end
+        ]], { i(1, "name"), i(2, "args"), i(0, "body") }
+      ),
     })
   ),
 }
