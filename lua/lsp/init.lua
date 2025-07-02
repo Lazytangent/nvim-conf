6267d6b1d0f856bd15ca57dcf01e3f32cc09246a
Peter Mai <peter.tn.mai@gmail.com>
2025-07-01 20:54:48 -0500

    Merge lsp config

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,9 +1,41 @@
-local utils = require("utils.core")
-
-local modules = {
-  "formatting",
-  "configure",
-  "efm",
+-- Formatting settings
+local format_options_prettier = {
+  tabWidth = 2,
+  singleQuote = true,
+  trailingComma = "all",
+  configPrecedence = "prefer-file"
 }
 
-utils.load_modules("lsp", modules)
+vim.g.format_options_typescript = format_options_prettier
+vim.g.format_options_javascript = format_options_prettier
+vim.g.format_options_typescriptreact = format_options_prettier
+vim.g.format_options_javascriptreact = format_options_prettier
+vim.g.format_options_json = {
+  tabWidth = 4,
+  singleQuote = false,
+  trailingComma = "all",
+  configPrecedence = "prefer-file"
+}
+vim.g.format_options_html = format_options_prettier
+vim.g.format_options_markdown = format_options_prettier
+vim.g.format_options_css = {
+  tabWidth = 2,
+  singleQuote = true,
+  trailingComma = "all",
+  configPrecedence = "prefer-file"
+}
+vim.g.format_options_yaml = format_options_prettier
+
+-- LSP Config
+local LSP = require("config").LSP
+
+vim.diagnostic.config {
+  virtual_text = LSP.virtual_text,
+  signs = true,
+  update_in_insert = false,
+}
+
+-- group, list
+for _, list in pairs(LSP.servers) do
+  vim.lsp.enable(list)
+end
