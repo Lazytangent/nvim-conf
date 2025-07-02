55159c0e44e4f90bef86a4a39778f1f5a8c288b5
Peter Mai <peter.tn.mai@gmail.com>
2025-07-01 20:52:45 -0500

    Migrate efm lsp settings

---
+++ lsp/efm.lua
@@ -0,0 +1,43 @@
+local capabilities = require("lsp.capabilities")
+local on_attach = require("lsp.regular.on_attach")
+
+local autopep8 = require("lsp.efm.autopep8")
+local black = require("lsp.efm.black")
+local eslint = require("lsp.efm.eslint")
+local isort = require("lsp.efm.isort")
+local prettier = require("lsp.efm.prettier")
+
+-- @type vim.lsp.Config
+return {
+  capabilities = capabilities,
+  on_attach = on_attach,
+  init_options = { documentFormatting = true },
+  root_dir = vim.loop.cwd,
+  filetypes = {
+    "python",
+    "json",
+    "javascript",
+    "typescript",
+    "html",
+    "css",
+    "yaml",
+    "markdown",
+    "javascriptreact",
+    "typescriptreact",
+  },
+  settings = {
+    rootMarkers = { ".git/" },
+    languages = {
+      python = { isort, autopep8, black },
+      typescript = { prettier, eslint },
+      javascript = { prettier, eslint },
+      typescriptreact = { prettier, eslint },
+      javascriptreact = { prettier, eslint },
+      yaml = { prettier },
+      json = { prettier },
+      html = { prettier },
+      css = { prettier },
+      markdown = { prettier },
+    },
+  },
+}
