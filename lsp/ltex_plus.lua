63ac232079b120f361e0f5bc3fd7a2aa627454d1
Peter Mai <peter.tn.mai@gmail.com>
2025-08-11 08:22:19 -0500

    Update ltex_plus lsp settings

--- lsp/ltex_plus.lua
+++ lsp/ltex_plus.lua
@@ -2,6 +2,16 @@ return {
   settings = {
     ltex = {
       language = "en-US",
+      dictionary = {
+        ["en-US"] = {
+          -- Use Neovim's existing spell list
+          ":~/.config/nvim/spell/en.utf-8.add",
+        },
+      },
+
+      -- default is:
+      -- checkFreqency = "edit",
+      checkFrequency = "save",
     },
   },
 }
