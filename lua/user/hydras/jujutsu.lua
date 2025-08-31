0076ad1a3625ae8204a6ecacdcdbceaecb93f886
Peter Mai <peter.tn.mai@gmail.com>
2025-08-30 21:22:20 -0500

    Update hydra for jujutsu

--- lua/user/hydras/jujutsu.lua
+++ lua/user/hydras/jujutsu.lua
@@ -2,16 +2,34 @@ local Hydra = require('hydra')
 
 Hydra({
   hint = [[
+  _b_ Bookmarks
+  _d_ Describe
   _l_ Log
+  _s_ Status
   ]],
   config = {
-    invoke_on_body= true,
+    invoke_on_body = true,
   },
   body = "<leader>jk",
   heads = {
     {
       'l',
-      function () vim.notify('Log') end,
+      function() vim.notify('Log') end,
+      { exit = true },
+    },
+    {
+      'b',
+      function() vim.notify('Bookmarks') end,
+      { exit = true },
+    },
+    {
+      'd',
+      function() vim.notify('Describe') end,
+      { exit = true },
+    },
+    {
+      's',
+      function() vim.notify('Status') end,
       { exit = true },
     },
   },
