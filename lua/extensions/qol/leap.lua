6f7c1b4130270eeb20677ff7027d25f97d66ee41
Peter Mai <peter.tn.mai@gmail.com>
2025-08-30 20:00:08 -0500

    Add flit.nvim

--- lua/extensions/qol/leap.lua
+++ lua/extensions/qol/leap.lua
@@ -1,6 +1,12 @@
 return {
-  'ggandor/leap.nvim',
-  config = function()
-    require('leap').set_default_mappings()
-  end,
+  {
+    'ggandor/leap.nvim',
+    config = function()
+      require('leap').set_default_mappings()
+    end,
+  },
+  {
+    'ggandor/flit.nvim',
+    config = true,
+  },
 }
