f5215873238eefbc1c823f09af3339891e08afd5
Peter Mai <peter.tn.mai@gmail.com>
2025-08-31 15:55:48 -0500

    Move more from telescope to mini.pick

--- lua/mappings/normal/leader/h.lua
+++ lua/mappings/normal/leader/h.lua
@@ -1,7 +1,5 @@
-local telescope = require 'telescope.builtin'
-
 return {
   { "<leader>h",  group = "Help" },
-  { "<leader>hh", telescope.help_tags, desc = "Help Tags" },
-  { "<leader>hk", telescope.keymaps,   desc = "Keymaps" },
+  { "<leader>hh", MiniPick.builtin.help, desc = "Help Tags" },
+  { "<leader>hk", MiniPick.builtin.keymaps,   desc = "Keymaps" },
 }
