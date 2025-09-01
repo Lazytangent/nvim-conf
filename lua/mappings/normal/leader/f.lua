e1aa14480f0ab71173165d959bb35dd85b9b5871
Peter Mai <peter.tn.mai@gmail.com>
2025-08-31 22:14:51 -0500

    Migrate private config picker

--- lua/mappings/normal/leader/f.lua
+++ lua/mappings/normal/leader/f.lua
@@ -4,17 +4,17 @@ local telescope_extensions = require('telescope').extensions
 local utils = require 'user.utils'
 
 local pickers = {
-  search_commands = function()
+  commands = function()
     MiniExtra.pickers.history({ scope = ':' })
   end,
   search_history = function()
     MiniExtra.pickers.history({ scope = '?' })
   end,
-  search_quickfix = function()
+  quickfix = function()
     MiniExtra.pickers.list({ scope = 'quickfix' })
   end,
-  search_jumplist = function()
-    MiniExtra.pickers.list({ scope = 'jumplist' })
+  jumplist = function()
+    MiniExtra.pickers.list({ scope = 'jump' })
   end,
   grep_this_word = function()
     MiniPick.builtin.grep({ pattern = vim.fn.expand('<cword>') })
@@ -26,6 +26,13 @@ local pickers = {
           char = '<CR>',
           func = utils.open_file_action,
         },
+      },
+    })
+  end,
+  config = function()
+    MiniPick.builtin.files(nil, {
+      source = {
+        cwd = vim.fn.stdpath('config'),
       },
     })
   end,
@@ -66,7 +73,6 @@ return {
   { "<leader>fB", require('extensions.telescope.custom.java').build_files, desc = "Java Build Files" },
   { "<leader>fJ", require('extensions.telescope.custom.java').files,       desc = "Java Files" },
   { "<leader>fL", telescope_extensions.luasnip.luasnip,                    desc = "Luasnip" },
-  { "<leader>fP", require('utils.core').search_nvim,                       desc = "Private config" },
 
   { "<leader>fo", telescope.quickfixhistory, desc = "Quickfix History" },
   { "<leader>fp", telescope.pickers,         desc = "Pickers" },
@@ -76,31 +82,32 @@ return {
   { "<leader>fD", lsp_pickers.definition,       desc = "LSP Definitions" },
   { "<leader>fG", git.modified,                 desc = "Git" },
   { "<leader>fO", MiniExtra.pickers.oldfiles,   desc = "Old files (recent)" },
+  { "<leader>fP", pickers.config,               desc = "Private config" },
   { "<leader>fR", MiniExtra.pickers.registers,  desc = "Registers" },
   { "<leader>fS", lsp_pickers.document_symbol,  desc = "Symbols" },
   { "<leader>fT", MiniExtra.pickers.treesitter, desc = "Treesitter" },
 
   { "<leader>fb", MiniPick.builtin.buffers,     desc = "Buffers" },
   { "<leader>fc", MiniExtra.pickers.commands,   desc = "Commands" },
-  { "<leader>fd", MiniExtra.pickers.dianostic,  desc = "Local diagnostics" },
+  { "<leader>fd", MiniExtra.pickers.diagnostic, desc = "Local diagnostics" },
   { "<leader>ff", pickers.files,                desc = "Files" },
   { "<leader>fg", MiniPick.builtin.grep_live,   desc = "Live grep" },
   { "<leader>fh", MiniPick.builtin.help,        desc = "Help tags" },
   { "<leader>fi", lsp_pickers.implementation,   desc = "LSP Implementations" },
-  { "<leader>fj", pickers.search_jumplist,      desc = "Jumplist" },
+  { "<leader>fj", pickers.jumplist,             desc = "Jumplist" },
   { "<leader>fk", MiniExtra.pickers.keymaps,    desc = "Keymaps" },
   { "<leader>fm", MiniExtra.pickers.marks,      desc = "Marks" },
-  { "<leader>fq", pickers.search_quickfix,      desc = "Quickfix" },
+  { "<leader>fq", pickers.quickfix,             desc = "Quickfix" },
   { "<leader>fr", lsp_pickers.references,       desc = "LSP References" },
   { "<leader>ft", pickers.grep_this_word,       desc = "This word" },
 
   { "<leader>f'", MiniPick.builtin.resume, desc = "Resume" },
-  { "<leader>f;", pickers.search_commands, desc = "Command history" },
+  { "<leader>f;", pickers.commands, desc = "Command history" },
   { "<leader>f?", pickers.search_history,  desc = "Search history" },
 
   { "<leader>fl",  group = "LSP Stuff" },
-  { "<leader>fld", telescope.diagnostics,          desc = "Diagnostics" },
-  { "<leader>fls", telescope.lsp_document_symbols, desc = "LSP Document Symbols" },
+  { "<leader>fld", MiniExtra.pickers.diagnostic, desc = "Diagnostics" },
+  { "<leader>fls", lsp_pickers.document_symbol,  desc = "LSP Document Symbols" },
 
   { "<leader>fs",  "<cmd>w<cr>", desc = "Save file" },
 }
