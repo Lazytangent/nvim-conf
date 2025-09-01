0e950e58c91d670a039fe4e07e47b6f3af4e659d
Peter Mai <peter.tn.mai@gmail.com>
2025-09-01 09:17:19 -0500

    Add set_quickfixlist helper binding for mini.pick pickers

--- lua/extensions/qol/mini.lua
+++ lua/extensions/qol/mini.lua
@@ -1,3 +1,31 @@
+local set_quickfixlist = function()
+  local matches = MiniPick.get_picker_matches()
+  local list = {}
+
+  -- Only works for files
+  -- See mini.pick source for how to handle other types based on
+  -- `default_choose_marked`
+  for _, match in ipairs(matches.all) do
+    local entry = {
+      filename = match,
+    }
+    table.insert(list, entry)
+  end
+  local list_data = {
+    items = list,
+    title = 'List',
+    nr = '$',
+  }
+
+  vim.fn.setqflist({}, ' ', list_data)
+  vim.cmd.copen()
+  vim.api.nvim_win_call(
+    MiniPick.get_picker_state().windows.target,
+    function() vim.cmd.crewind() end
+  )
+  return true
+end
+
 -- will allow me to `require` configs per module later
 local configs = {
   ai = true,
@@ -7,6 +35,10 @@ local configs = {
   pick = {
     mappings = {
       choose_marked = '<C-CR>',
+      set_quickfixlist = {
+        char = '<C-q>',
+        func = set_quickfixlist,
+      },
     },
     options = {
       content_from_bottom = true,
