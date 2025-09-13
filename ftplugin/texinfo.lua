5f624a33b6a30c14db5271785b689fe0994a529b
Peter Mai <peter.tn.mai@gmail.com>
2025-09-13 11:33:33 -0500

    Set makeprg for texinfo files

--- ftplugin/texinfo.lua
+++ ftplugin/texinfo.lua
@@ -1,2 +1,4 @@
 vim.opt_local.commentstring = '@c%s'
 vim.opt_local.errorformat = '%f:%l: %tarning: %m,%f:%l: %m,%-G%.%#'
+
+vim.opt_local.makeprg = "makeinfo --pdf % --output=" .. vim.fn.expand("%:p:h") .. "/" .. vim.fn.expand("%:t:r") .. ".pdf"
