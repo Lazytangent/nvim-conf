a154bcb934e147b69b86bfea11a2a7292918315f
Peter Mai <peter.tn.mai@gmail.com>
2025-09-13 11:35:13 -0500

    Add Make user command for texinfo files to have backup for makefile make

--- ftplugin/texinfo.lua
+++ ftplugin/texinfo.lua
@@ -2,3 +2,5 @@ vim.opt_local.commentstring = '@c%s'
 vim.opt_local.errorformat = '%f:%l: %tarning: %m,%f:%l: %m,%-G%.%#'
 
 vim.opt_local.makeprg = "makeinfo --pdf % --output=" .. vim.fn.expand("%:p:h") .. "/" .. vim.fn.expand("%:t:r") .. ".pdf"
+
+vim.api.nvim_buf_create_user_command(0, 'Make', "<cmd>!make<cr>", {})
