41db037bdaf230053b49b27b08820170d5e9836c
Peter Mai <peter.tn.mai@gmail.com>
2025-09-22 18:35:54 -0500

    Update zls to work with anyzig install

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -3,4 +3,9 @@ local capabilities = require('lsp.capabilities')
 -- @type vim.lsp.Config
 return {
   capabilities = capabilities,
+  settings = {
+    zls = {
+      zig_exe_path = vim.fn.expand("~/") .. ".local/bin/zigm",
+    },
+  },
 }
