014f2ca34fe6aeaf705fa44a00908fb1a2a98bd5
Peter Mai <peter.tn.mai@gmail.com>
2025-09-23 16:43:43 -0500

    More customization of zls

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -4,7 +4,15 @@ local capabilities = require('lsp.capabilities')
 return {
   capabilities = capabilities,
   settings = {
+    -- https://github.com/zigtools/zls/blob/master/schema.json
     zls = {
+      semantic_tokens = "partial",
+      inlay_hints_show_variable_type_hints = false,
+      inlay_hints_show_struct_literal_field_type = false,
+      inlay_hints_show_parameter_name = false,
+      inlay_hints_show_builtin = false,
+      warn_style = true,
+      skip_std_references = true,
       zig_exe_path = vim.fn.expand("~/") .. ".local/bin/zigm",
     },
   },
