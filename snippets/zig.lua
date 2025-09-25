b86345091a53daa21ef4c6fe9b97d7c0b55dbabb
Peter Mai <peter.tn.mai@gmail.com>
2025-09-25 14:20:37 -0500

    Add zig snippets

--- snippets/zig.lua
+++ snippets/zig.lua
@@ -3,6 +3,17 @@ local snippets = {
     { trig = "std" },
     fmta('const std = @import("std");<>', { i(0) })
   ),
+  s(
+    { trig = "test" },
+    fmta([[
+    test "<>" {
+        <>
+    }
+    ]], {
+      i(1, "name"),
+      i(0),
+    })
+  ),
 }
 
 return snippets
