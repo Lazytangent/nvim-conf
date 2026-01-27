fc680334c8570cd245a6af6a480a52b6ceda6c45
Peter Mai <peter.tn.mai@gmail.com>
2026-01-27 11:46:48 -0600

    Use nnn.vim instead

--- lua/extensions/qol/nnn.lua
+++ lua/extensions/qol/nnn.lua
@@ -1,6 +1,14 @@
 return {
+  -- {
+  --   'luukvbaal/nnn.nvim',
+  --   config = true,
+  -- },
+
   {
-    'luukvbaal/nnn.nvim',
-    config = true,
+    'mcchrish/nnn.vim',
+    init = function()
+      vim.g['nnn#set_default_mappings'] = 0
+      vim.g['nnn#layout'] = 'new'
+    end,
   },
 }
