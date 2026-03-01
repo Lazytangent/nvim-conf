e85941d2430e9816584a7034b12e0d6739e52a3a
Peter Mai <peter.tn.mai@gmail.com>
2026-03-01 16:21:26 -0600

    Remove debugging print

--- lsp/pyrefly.lua
+++ lsp/pyrefly.lua
@@ -3,28 +3,24 @@ local capabilities = require('lsp.capabilities')
 -- @type vim.lsp.Config
 return {
   capabilities = capabilities,
-  -- Initial attempt at setting pythonPath for lsp
-  -- TODO: Test cases
-  -- - uv venv
-  -- - poetry venv
-  -- - pipenv venv
-  -- - manual python -m venv
   settings = {
     python = {},
   },
   on_init = function(client)
-    vim.print(client)
     client.settings.python.pythonPath = (function(workspace)
       if vim.env.VIRTUAL_ENV then
         return vim.fs.joinpath(vim.env.VIRTUAL_ENV, "bin", "python")
       end
       if workspace then
         local poetry_lock_path = vim.fs.joinpath(workspace, "poetry.lock")
+        local pipfile_path = vim.fs.joinpath(workspace, "Pipfile")
         if vim.uv.fs_stat(poetry_lock_path) then
           local venv = vim.fn.trim(vim.fn.system "poetry env info -p")
           return vim.fs.joinpath(venv, "bin", "python")
+        elseif vim.uv.fs_stat(pipfile_path) then
+          local venv = vim.fn.trim(vim.fn.system "pipenv --venv")
+          return vim.fs.joinpath(venv, "bin", "python")
         end
-        -- TODO: What about pipenv?
       end
       return vim.fn.exepath "python3" or vim.fn.exepath "python" or "python"
     end)(client.config.root_dir)
