923beb01ce622f6110f292b52bd6260f5cb93cd6
Peter Mai <peter.tn.mai@gmail.com>
2026-02-28 22:39:08 -0600

    Update python configuration for lsps

--- lsp/pyrefly.lua
+++ lsp/pyrefly.lua
@@ -3,4 +3,30 @@ local capabilities = require('lsp.capabilities')
 -- @type vim.lsp.Config
 return {
   capabilities = capabilities,
+  -- Initial attempt at setting pythonPath for lsp
+  -- TODO: Test cases
+  -- - uv venv
+  -- - poetry venv
+  -- - pipenv venv
+  -- - manual python -m venv
+  settings = {
+    python = {},
+  },
+  on_init = function(client)
+    vim.print(client)
+    client.settings.python.pythonPath = (function(workspace)
+      if vim.env.VIRTUAL_ENV then
+        return vim.fs.joinpath(vim.env.VIRTUAL_ENV, "bin", "python")
+      end
+      if workspace then
+        local poetry_lock_path = vim.fs.joinpath(workspace, "poetry.lock")
+        if vim.uv.fs_stat(poetry_lock_path) then
+          local venv = vim.fn.trim(vim.fn.system "poetry env info -p")
+          return vim.fs.joinpath(venv, "bin", "python")
+        end
+        -- TODO: What about pipenv?
+      end
+      return vim.fn.exepath "python3" or vim.fn.exepath "python" or "python"
+    end)(client.config.root_dir)
+  end,
 }
--- lsp/uv_ty.lua
+++ lsp/uv_ty.lua
@@ -11,24 +11,28 @@ return {
     'requirements.txt',
     '.git',
   },
-  -- Initial attempt at setting pythonPath for lsp
-  -- TODO: Test cases
-  -- - uv venv
-  -- - poetry venv
-  -- - pipenv venv
-  -- - manual python -m venv
+  settings = {
+    ty = {
+      configuration = {
+        environment = {},
+      },
+    },
+  },
   on_init = function(client)
-    client.config.settings.python.pythonPath = (function(workspace)
+    client.settings.ty.configuration.environment.python = (function(workspace)
       if vim.env.VIRTUAL_ENV then
         return vim.fs.joinpath(vim.env.VIRTUAL_ENV, "bin", "python")
       end
       if workspace then
         local poetry_lock_path = vim.fs.joinpath(workspace, "poetry.lock")
-        if vim.fs.filereadable(poetry_lock_path) then
+        local pipfile_path = vim.fs.joinpath(workspace, "Pipfile")
+        if vim.uv.fs_stat(poetry_lock_path) then
           local venv = vim.fn.trim(vim.fn.system "poetry env info -p")
           return vim.fs.joinpath(venv, "bin", "python")
+        elseif vim.uv.fs_stat(pipfile_path) then
+          local venv = vim.fn.trim(vim.fn.system "pipenv --venv")
+          return vim.fs.joinpath(venv, "bin", "python")
         end
-        -- TODO: What about pipenv?
       end
       return vim.fn.exepath "python3" or vim.fn.exepath "python" or "python"
     end)(client.config.root_dir)
