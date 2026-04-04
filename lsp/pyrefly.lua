2c98b50a0d51ca1dc9b8dc7475d0412690a66ea4
Peter Mai <peter.tn.mai@gmail.com>
2026-04-04 09:13:10 -0500

    Reuse pythonPath function

--- lsp/pyrefly.lua
+++ lsp/pyrefly.lua
@@ -1,4 +1,5 @@
 local capabilities = require('lsp.capabilities')
+local get_python_path = require('user.utils').get_python_path
 
 -- @type vim.lsp.Config
 return {
@@ -7,22 +8,6 @@ return {
     python = {},
   },
   on_init = function(client)
-    client.settings.python.pythonPath = (function(workspace)
-      if vim.env.VIRTUAL_ENV then
-        return vim.fs.joinpath(vim.env.VIRTUAL_ENV, "bin", "python")
-      end
-      if workspace then
-        local poetry_lock_path = vim.fs.joinpath(workspace, "poetry.lock")
-        local pipfile_path = vim.fs.joinpath(workspace, "Pipfile")
-        if vim.uv.fs_stat(poetry_lock_path) then
-          local venv = vim.fn.trim(vim.fn.system "poetry env info -p")
-          return vim.fs.joinpath(venv, "bin", "python")
-        elseif vim.uv.fs_stat(pipfile_path) then
-          local venv = vim.fn.trim(vim.fn.system "pipenv --venv")
-          return vim.fs.joinpath(venv, "bin", "python")
-        end
-      end
-      return vim.fn.exepath "python3" or vim.fn.exepath "python" or "python"
-    end)(client.config.root_dir)
+    client.settings.python.pythonPath = get_python_path(client.config.root_dir)
   end,
 }
